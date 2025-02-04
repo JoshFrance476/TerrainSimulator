8bbbdfc2a9e74030fd9212578f3123c7af02734b
Josh France <joshfrance.476@gmail.com>
2025-02-04 20:04:39 +0000

    Introduced basic city territory growth logic

--- simulator_main.py
+++ simulator_main.py
@@ -1,22 +1,24 @@
 import numpy as np
 import config
 from generator_main import generate_initial_maps
-from overlay_generator import apply_heatmap_overlay, generate_coastline_map
+from overlay_generator import apply_heatmap_overlay, generate_coastline_map, generate_territory_overlay
 from find_top_desiribility import find_top_desiribility_points
-from City import City
+from AnchorPoint import AnchorPoint
 import random
 
 
 def simulate_init():
-    global desiribility_map, desiribility_display, colour_display, population_display, outline_display, elevation_display, traversal_cost_map, sea_map, territory_map, population_map
-    colour_display, desiribility_map, elevation_map, traversal_cost_map, sea_map = generate_initial_maps()
+    global desiribility_map, desiribility_display, colour_display, population_display, outline_display, elevation_display, traversal_cost_map, sea_map, river_map, territory_display, cities_list, population_map
+    colour_display, desiribility_map, elevation_map, traversal_cost_map, sea_map, river_map = generate_initial_maps()
     outline_display = generate_coastline_map(elevation_map)
     population_map = np.zeros((config.ROWS, config.COLS), dtype=float)
     population_map = update_population(population_map)
-    display_map, cities_list = generate_cities(population_map, colour_display)
 
+    AnchorPoint.initialise(traversal_cost_map, sea_map, river_map)
+    
+    initialise_cities(population_map)
 
-    territory_map = generate_territory_map(display_map, cities_list)
+    territory_display = generate_territory_overlay(colour_display, AnchorPoint.get_uid_map(), AnchorPoint.get_anchors())
     elevation_display = apply_heatmap_overlay(elevation_map, outline_display)
     population_display = apply_heatmap_overlay(population_map, outline_display, "inferno")
     desiribility_display = apply_heatmap_overlay(desiribility_map, outline_display, "inferno")
@@ -25,21 +27,21 @@ def simulate_init():
  
 
 def simulate_loop(filter):
-    global population_display, desiribility_display, elevation_display, traversal_cost_map, territory_map, population_map
+    global population_display, desiribility_display, elevation_display, traversal_cost_map, territory_display, population_map
     population_map = update_population(population_map)
-    mod_filter = filter % 5
+    mod_filter = filter % 4
 
     if mod_filter == 0:
         display_map = colour_display
     elif mod_filter == 1:
-        display_map = elevation_display
+        display_map = desiribility_display
     elif mod_filter == 2:
         display_map = population_display
     elif mod_filter == 3:
-        display_map = desiribility_display
-    elif mod_filter == 4:
-        display_map = territory_map
-    
+        for anchor in AnchorPoint.get_anchors():
+            anchor.update_territory_size()
+        AnchorPoint.generate_territory_uid_map()
+        display_map = generate_territory_overlay(colour_display, AnchorPoint.get_uid_map(), AnchorPoint.get_anchors())
 
     return display_map
 
@@ -54,30 +56,20 @@ def update_population(population_map):
     return population_map
 
 
-def generate_cities(population_map, colour_display):
-    cities_map = colour_display.copy()
-    cities_list = []  # Initialize a list to store City objects
+#Returns list of anchor objects located at the top n desiribility points
+def initialise_cities(population_map):
 
-    # Find city locations
-    cities_location_list = find_top_desiribility_points(population_map, config.NUMBER_OF_CITIES, config.CITIES_MIN_DISTANCE)
+    # Find anchor locations
+    anchor_location_list = find_top_desiribility_points(population_map, config.NUMBER_OF_CITIES, config.CITIES_MIN_DISTANCE)
 
-    # Apply black mask to each city location and store City objects
-    for _, (r, c) in cities_location_list:
-        cities_map[r, c] = [0, 0, 0]  # Set pixel to black
-        cities_list.append(City((r, c),random.randint(*config.CITY_PROSPERITY_RANGE)))  # Store City object in the list
+    for uid, (_, (r, c)) in enumerate(anchor_location_list):
+        AnchorPoint((r, c),random.randint(*config.CITY_PROSPERITY_RANGE), uid+1)
+    
+    AnchorPoint.generate_territory_uid_map()
+        
 
-    return cities_map, cities_list  # Return updated map and list of City objects
 
-def generate_territory_map(display_map, cities_list):
-    territory_map = display_map.copy()
-    for city in cities_list:
-        city.generate_territory(traversal_cost_map, sea_map)
-        city_colour = city.get_colour()
-        for (r, c) in city.get_territory():
-            territory_map[r, c] = city_colour  # Set the city's color in its territory
-            if (r, c) == city.get_location():
-                territory_map[r, c] = [0,0,0]
-    return territory_map
+
     
 
 
