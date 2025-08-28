3fde39968c378567464abdc4bc1a416a4c55c2c3
Josh France <joshfrance.476@gmail.com>
2025-08-28 16:19:18 +0100

    Refactored class to make it more modular

--- rendering/map_renderer.py
+++ rendering/map_renderer.py
@@ -9,25 +9,29 @@ class MapRenderer:
         self.surface_cache = None  # Caching pre-rendered terrain for optimization
         self.current_step = 0
 
-    def draw_map(self, screen, world_data, map_filter, camera):
+    def render_view(self, screen, world_data, map_filter, camera):
+        '''Takes in world data and filter to produce display map, then draws to screen'''
+        display_map = self.apply_overlay(world_data, map_filter)
+        self.draw_map_to_screen(screen, display_map, camera)
+
+    
+    def apply_overlay(self, world_data, map_filter):
+        '''Applies selected overlay to the base colour map.'''
         static_maps, dynamic_maps, display_map = world_data
-        """Draws the terrain map and overlays."""
-        screen.fill((0, 0, 0))  # Clear screen
-        
-        camera.clamp_pan()     
 
         if map_filter == 1:
             display_map = apply_heatmap_overlay(
                     dynamic_maps["population_map"],
                     static_maps["elevation_map"]
                 )
-
-
-        self.draw_terrain(screen, display_map, camera)
+        return display_map
 
 
     
-    def draw_terrain(self, screen, display_map, camera):
+    def draw_map_to_screen(self, screen, display_map, camera):
+        '''Draws the given display map to the screen based on camera position.'''
+        screen.fill((0, 0, 0))
+
         for r in range(config.CAMERA_ROWS):
             for c in range(config.CAMERA_COLS):
                 colour = display_map[r+camera.y_pos][c+camera.x_pos]
