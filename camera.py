4a112592c10eb379010f13624159e397f040cd8c
Josh France <joshfrance.476@gmail.com>
2025-08-28 16:21:55 +0100

    Added comment describing the camera x and y variables

--- camera.py
+++ camera.py
@@ -2,7 +2,7 @@ from utils import config
 
 class Camera:
     def __init__(self):
-        self.x_pos, self.y_pos = 0, 0
+        self.x_pos, self.y_pos = 0, 0  #Top-left corner of the camera view, in cell coordinates
 
     # Ensure camera does not pan beyond world boundaries
     def clamp_pan(self):
