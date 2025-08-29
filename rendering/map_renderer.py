35881d9dbecd7bd44cc577955442f400fc3c0efd
Josh France <joshfrance.476@gmail.com>
2025-08-29 16:22:14 +0100

    Handles refactored world data correctly

--- rendering/map_renderer.py
+++ rendering/map_renderer.py
@@ -9,21 +9,23 @@ class MapRenderer:
         self.surface_cache = None  # Caching pre-rendered terrain for optimization
         self.current_step = 0
 
-    def render_view(self, screen, world_data, map_filter, camera):
+    def render_view(self, screen, world_data, terrain_data, map_filter, camera):
         '''Takes in world data and filter to produce display map, then draws to screen'''
-        display_map = self.apply_overlay(world_data, map_filter)
+        display_map = self.apply_overlay(world_data, terrain_data, map_filter)
         self.draw_map_to_screen(screen, display_map, camera)
 
     
-    def apply_overlay(self, world_data, map_filter):
+    def apply_overlay(self, world_data, terrain_data, map_filter):
         '''Applies selected overlay to the base colour map.'''
-        static_maps, dynamic_maps, display_map = world_data
 
         if map_filter == 1:
             display_map = apply_heatmap_overlay(
-                    dynamic_maps["population_map"],
-                    static_maps["elevation_map"]
+                    #world_data["population"],
+                    world_data["elevation"]
                 )
+        else:
+            display_map = terrain_data
+
         return display_map
 
 
