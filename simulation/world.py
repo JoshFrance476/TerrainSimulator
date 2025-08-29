12f6ce6c53a17725ef81be68b61af122f8b66bdf
Josh France <joshfrance.476@gmail.com>
2025-08-29 21:29:47 +0100

    Moved all functionality to WorldData, this class will come back into use later

--- simulation/world.py
+++ simulation/world.py
@@ -1,38 +1,16 @@
-from utils.colour_utils import generate_color_map
-from generation.generator_main import generate_static_maps, generate_dynamic_maps, update_dynamic_maps
-
-
-
+from worldData import WorldData
 
 class World:
     """Handles world data generation and updates."""
-    def __init__(self):
-        self.static_maps_dict = generate_static_maps()
-        self.dynamic_maps_dict = generate_dynamic_maps(self.static_maps_dict)
-
-        #Generates initial colour map
-        self.terrain_map = generate_color_map(self.static_maps_dict, True, True)
 
 
-    def update(self):
-        self.dynamic_maps = update_dynamic_maps(self.static_maps_dict, self.dynamic_maps_dict)
+    #def update(self):
+        #self.dynamic_maps = update_dynamic_maps(self.static_maps_dict, self.dynamic_maps_dict)
 
 
-    def get_world_data(self):
-        return self.static_maps_dict, self.dynamic_maps_dict
-    
-    def get_terrain_data(self):
-        return self.terrain_map
 
 
-    def get_terrain_map(self):
-        return self.terrain_map
-    
-    def get_static_maps(self):
-        return self.static_maps
 
-    def get_dynamic_maps(self):
-        return self.dynamic_maps
 
 
 
