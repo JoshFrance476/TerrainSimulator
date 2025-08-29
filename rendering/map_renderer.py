21bb1d8e791f052a90006424784914e8d6558ce1
Josh France <joshfrance.476@gmail.com>
2025-08-29 22:16:00 +0100

    Added AI pygame rendering function which is much more efficient at rendering pixels

--- rendering/map_renderer.py
+++ rendering/map_renderer.py
@@ -8,40 +8,31 @@ class MapRenderer:
     def __init__(self):
         pass
     
-
-    def generate_static_overlays(self, world_data):
-        self.pop_capacity_overlay = apply_heatmap_overlay(
-                    world_data["population_capacity"],
-                    world_data["elevation"]
-                )
     
-    def render_view(self, screen, terrain_data, map_filter, camera):
+    def render_view(self, screen, view_data, map_filter):
         '''Takes in world data and filter to produce display map, then draws to screen'''
-        display_map = self.apply_overlay(terrain_data, map_filter)
-        self.draw_map_to_screen(screen, display_map, camera)
+        display_map = self.apply_overlay(view_data, map_filter)
+        self.draw_map_to_screen(screen, display_map)
 
     
-    def apply_overlay(self, terrain_data, map_filter):
+    def apply_overlay(self, view_data, map_filter):
         '''Applies selected overlay to the base colour map.'''
 
         if map_filter == 1:
-            display_map = self.pop_capacity_overlay
+            display_map = apply_heatmap_overlay(
+                    view_data["population_capacity"],
+                    view_data["elevation"]
+                )
         else:
-            display_map = terrain_data
+            display_map = view_data['terrain']
 
         return display_map
 
 
     
-    def draw_map_to_screen(self, screen, display_map, camera):
-        '''Draws the given display map to the screen based on camera position.'''
-        screen.fill((0, 0, 0))
-
-        for r in range(config.CAMERA_ROWS):
-            for c in range(config.CAMERA_COLS):
-                colour = display_map[r+camera.y_pos][c+camera.x_pos]
-                pygame.draw.rect(
-                    screen,
-                    colour,
-                    (c * config.CELL_SIZE, r * config.CELL_SIZE, config.CELL_SIZE, config.CELL_SIZE)
-                )
+    def draw_map_to_screen(self, screen, display_map):
+        """AI code using surfarray to draw the whole map at once."""
+        surface = pygame.surfarray.make_surface(display_map.swapaxes(0, 1))  
+        surface = pygame.transform.scale(surface, (display_map.shape[1] * config.CELL_SIZE,
+                                                display_map.shape[0] * config.CELL_SIZE))
+        screen.blit(surface, (0, 0))
