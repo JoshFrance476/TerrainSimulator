2e525542c75e0c3ea1056fd0c8a42b8bf67f91c3
Josh France <joshfrance.476@gmail.com>
2025-08-30 10:57:38 +0100

    coastline map is now passed in directly from WorldData instead of passing in elevation map and calculating coastline

--- rendering/map_renderer.py
+++ rendering/map_renderer.py
@@ -1,13 +1,12 @@
 import pygame
 from utils import config
-from overlays.overlay_generator import generate_territory_overlay, apply_heatmap_overlay
+from overlays.overlay_generator import apply_heatmap_overlay
 
 class MapRenderer:
     """Handles rendering the terrain and overlays on the screen."""
 
     def __init__(self):
         pass
-    
     
     def render_view(self, screen, view_data, map_filter):
         '''Takes in world data and filter to produce display map, then draws to screen'''
@@ -20,8 +19,8 @@ class MapRenderer:
 
         if map_filter == 1:
             display_map = apply_heatmap_overlay(
-                    view_data["population_capacity"],
-                    view_data["elevation"]
+                    view_data["coastline"],
+                    view_data["population_capacity"]
                 )
         else:
             display_map = view_data['terrain']
