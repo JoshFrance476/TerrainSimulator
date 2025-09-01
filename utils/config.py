3f68e390adcd3d88a9f5689f24f63f26d1455dc2
Josh France <joshfrance.476@gmail.com>
2025-09-01 21:45:04 +0100

    Regions are now referred to by ID instead of strings, using a lookup table to convert between them

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 CAMERA_ROWS, CAMERA_COLS = 266, 466 
 WORLD_ROWS, WORLD_COLS = CAMERA_ROWS * 2, CAMERA_COLS * 2
@@ -21,49 +22,75 @@ PAN_STEP = 4    # Pan speed in pixels
 SIDEBAR_WIDTH = 250  
 FONT_SIZE = 18
 
+REGION_LOOKUP = {
+    "water": 0,
+    "snowy peaks": 1,
+    "mountains": 2,
+    "desert": 3,
+    "arid": 4,
+    "grassland": 5,
+    "forest": 6,
+    "savannah": 7,
+    "tundra": 8,
+    "marsh": 9
+}
+
+REGION_NAMES = {v: k for k, v in REGION_LOOKUP.items()}
+
 
 REGION_CONDITIONS = [
-    {"condition": lambda e, t, r, rp: e > 0.35, "color": "snowy peaks"},
-    {"condition": lambda e, t, r, rp: e > 0.25, "color": "mountains"},
-    {"condition": lambda e, t, r, rp: t < 0.02, "color": "tundra"},
-    {"condition": lambda e, t, r, rp: e < 0.1 and t > 0.76 and r < 0.4, "color": "desert"},
-    {"condition": lambda e, t, r, rp: e < 0.25 and t > 0.62 and r < 0.65 and rp >= 4, "color": "arid"},
-    {"condition": lambda e, t, r, rp: e < 0.23 and t > 0.52 and (r > 0.5 or rp < 4), "color": "savannah"},
-    {"condition": lambda e, t, r, rp: e < 0.05 and t < 0.45 and t > 0.2 and r > 0.6, "color": "marsh"},
-    {"condition": lambda e, t, r, rp: r > 0.50, "color": "forest"},
-    {"condition": lambda e, t, r, rp: True, "color": "grassland"},
+    {"condition": lambda e, t, r, rp: (e > 0.35), 
+     "regionID": REGION_LOOKUP["snowy peaks"]},
+    {"condition": lambda e, t, r, rp: (e > 0.25), 
+     "regionID": REGION_LOOKUP["mountains"]},
+    {"condition": lambda e, t, r, rp: (t < 0.02), 
+     "regionID": REGION_LOOKUP["tundra"]},
+    {"condition": lambda e, t, r, rp: (e < 0.1) & (t > 0.76) & (r < 0.4), 
+     "regionID": REGION_LOOKUP["desert"]},
+    {"condition": lambda e, t, r, rp: (e < 0.25) & (t > 0.62) & (r < 0.65) & (rp >= 4), 
+     "regionID": REGION_LOOKUP["arid"]},
+    {"condition": lambda e, t, r, rp: (e < 0.23) & (t > 0.52) & ((r > 0.5) | (rp < 4)), 
+     "regionID": REGION_LOOKUP["savannah"]},
+    {"condition": lambda e, t, r, rp: (e < 0.05) & (t < 0.45) & (t > 0.2) & (r > 0.6), 
+     "regionID": REGION_LOOKUP["marsh"]},
+    {"condition": lambda e, t, r, rp: (r > 0.50), 
+     "regionID": REGION_LOOKUP["forest"]},
+     {"condition": lambda e, t, r, rp: True, 
+      "regionID": REGION_LOOKUP["grassland"]},
 ]
 
+
+
 REGION_COLORS = {
-    "water": (0, 0, 180),
-    "snowy peaks": (226, 226, 226),
-    "mountains": (130, 130, 130),
-    "desert": (194, 140, 70),
-    "arid": (190, 160, 130),
-    "grassland": (34, 139, 34),
-    "forest": (34, 112, 34),
-    "savannah": (125, 160, 50),
-    "tundra": (230, 230, 230),
-    "marsh": (0, 90, 0)
+    0: (0, 0, 180),
+    1: (226, 226, 226),
+    2: (130, 130, 130),
+    3: (194, 140, 70),
+    4: (190, 160, 130),
+    5: (34, 139, 34),
+    6: (34, 112, 34),
+    7: (125, 160, 50),
+    8: (230, 230, 230),
+    9: (0, 90, 0)
 }
 
-REGION_BASE_TRAVERSAL_COST = {
-    "water": 10,
-    "snowy peaks": 10,
-    "mountains": 10,
-    "desert": 1,
-    "arid": 1,
-    "grassland": 1,
-    "forest": 1.2,
-    "savannah": 1,
-    "tundra": 2,
-    "marsh": 2
-}
+REGION_BASE_TRAVERSAL_COST = np.array([
+    10,  # 0 grassland
+    10,  # 1 forest
+    10,  # 2 desert
+    1,   # 3 mountains
+    1,   # 4 snowy peaks
+    1,   # 5 tundra
+    1,   # 6 savannah
+    1,   # 7 arid
+    2,   # 8 marsh
+    2    # 9 water
+], dtype=np.uint8)
 
 REGIONS_TO_BLEND = {
-    "desert": ["arid","savannah"],
-    "arid": ["desert","savannah"],
-    "grassland": ["forest", "marsh"],
-    "forest": ["grassland", "marsh"],
-    "savannah": ["desert","arid"]
+    3: [4,7],
+    4: [3,7],
+    5: [6, 9],
+    6: [5, 9],
+    7: [3,4]
 }
