a42bb3ac22a17201bbbe3b2801cd9ed2251e6a86
Josh France <joshfrance.476@gmail.com>
2025-09-01 15:50:41 +0100

    Added heatmaps for all different data from WorldData

--- rendering/map_renderer.py
+++ rendering/map_renderer.py
@@ -6,7 +6,7 @@ class MapRenderer:
     """Handles rendering the terrain and overlays on the screen."""
 
     def __init__(self):
-        pass
+        self.selected_filter_name = "None"
     
     def render_view(self, screen, view_data, map_filter):
         '''Takes in world data and filter to produce display map, then draws to screen'''
@@ -20,10 +20,48 @@ class MapRenderer:
         if map_filter == 1:
             display_map = apply_heatmap_overlay(
                     view_data["coastline"],
+                    view_data["elevation"]
+                )
+            self.selected_filter_name = "Elevation"
+        elif map_filter == 2:
+            display_map = apply_heatmap_overlay(
+                    view_data["coastline"],
+                    view_data["temperature"]
+                )
+            self.selected_filter_name = "Temperature"
+        elif map_filter == 3:
+            display_map = apply_heatmap_overlay(
+                    view_data["coastline"],
+                    view_data["rainfall"]
+                )
+            self.selected_filter_name = "Rainfall"
+        elif map_filter == 4:
+            display_map = apply_heatmap_overlay(
+                    view_data["coastline"],
                     view_data["population_capacity"]
                 )
+            self.selected_filter_name = "Population Capacity"
+        elif map_filter == 5:
+            display_map = apply_heatmap_overlay(
+                    view_data["coastline"],
+                    view_data["fertility"]
+                )
+            self.selected_filter_name = "Fertility"
+        elif map_filter == 6:
+            display_map = apply_heatmap_overlay(
+                    view_data["coastline"],
+                    view_data["traversal_cost"]
+                )
+            self.selected_filter_name = "Traversal Cost"
+        elif map_filter == 7:
+            display_map = apply_heatmap_overlay(
+                    view_data["coastline"],
+                    view_data["steepness"]
+                )
+            self.selected_filter_name = "Steepness"
         else:
             display_map = view_data['terrain']
+            self.selected_filter_name = "None"
 
         return display_map
 
@@ -35,3 +73,6 @@ class MapRenderer:
         surface = pygame.transform.scale(surface, (display_map.shape[1] * config.CELL_SIZE,
                                                 display_map.shape[0] * config.CELL_SIZE))
         screen.blit(surface, (0, 0))
+
+    def get_selected_filter_name(self):
+        return self.selected_filter_name
