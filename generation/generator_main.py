2825d7001fa3c76c8f99a0e7ccbef1ad022b7229
Josh France <joshfrance.476@gmail.com>
2025-09-02 17:00:33 +0100

    rows and cols are passed in instead of relying on config file for better modular design

--- generation/generator_main.py
+++ generation/generator_main.py
@@ -13,17 +13,17 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 
-def generate_static_maps():
+def generate_static_maps(rows, cols):
     start_time = time.time()
-    elevation_map, rainfall_map, temperature_map = generate_stage_1(config.WORLD_ROWS, config.WORLD_COLS, config.SCALE, config.SEED)
+    elevation_map, rainfall_map, temperature_map = generate_stage_1(rows, cols, config.SCALE, config.SEED)
     logging.debug(f"Stage 1 generation took {time.time() - start_time:.2f} seconds")
 
     start_time = time.time()
-    river_map, sea_map, steepness_map, coastline_map = generate_stage_2(config.WORLD_ROWS, config.WORLD_COLS, config.NUMBER_OF_RIVERS, config.SEA_LEVEL, elevation_map, config.RIVER_SOURCE_MIN_ELEVATION)
+    river_map, sea_map, steepness_map, coastline_map = generate_stage_2(config.NUMBER_OF_RIVERS, config.SEA_LEVEL, elevation_map, config.RIVER_SOURCE_MIN_ELEVATION)
     logging.debug(f"Stage 2 generation took {time.time() - start_time:.2f} seconds")
     
     start_time = time.time()
-    river_proximity_map, sea_proximity_map, region_map, fertility_map, traversal_cost_map = generate_stage_3(config.WORLD_ROWS, config.WORLD_COLS, river_map, sea_map, elevation_map, temperature_map, rainfall_map, steepness_map, config.REGION_CONDITIONS, config.REGION_COLORS, config.REGIONS_TO_BLEND)
+    river_proximity_map, sea_proximity_map, region_map, fertility_map, traversal_cost_map = generate_stage_3(river_map, sea_map, elevation_map, temperature_map, rainfall_map, steepness_map, config.REGION_CONDITIONS)
     logging.debug(f"Stage 3 generation took {time.time() - start_time:.2f} seconds")
 
     
@@ -36,7 +36,7 @@ def generate_static_maps():
 
 def generate_dynamic_maps(static_data):
     start_time = time.time()
-    population_capacity_map, population_map = generate_stage_4(config.WORLD_ROWS, config.WORLD_COLS, static_data)
+    population_capacity_map, population_map = generate_stage_4(static_data)
     logging.debug(f"Stage 4 generation took {time.time() - start_time:.2f} seconds")
 
     return population_capacity_map, population_map
