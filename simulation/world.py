960051c731550965255d54cca18c1f30fef96a4f
Josh France <joshfrance.476@gmail.com>
2025-09-02 17:03:40 +0100

    Set up World class to handle high level simulator logic

--- simulation/world.py
+++ simulation/world.py
@@ -1,8 +1,14 @@
 from worldData import WorldData
 
 class World:
-    """Handles world data generation and updates."""
+    """Handles simulator and high level world logic."""
 
+    def __init__(self, rows, cols):
+        self.rows, self.cols = rows, cols
+        self.data = WorldData(rows, cols)
+    
+    def step(self):
+        self.data.update()
 
     #def update(self):
         #self.dynamic_maps = update_dynamic_maps(self.static_maps_dict, self.dynamic_maps_dict)
