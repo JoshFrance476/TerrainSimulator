3bc72763b47e1678a06013febe04a073c98d6a81
Josh France <joshfrance.476@gmail.com>
2025-09-03 13:41:22 +0100

    Settlements are now only handled as objects, and population map is passed in as reference to settlement objects

--- SettlementManager.py
+++ SettlementManager.py
@@ -7,31 +7,31 @@ class SettlementManager:
         self.settlements = {}
         self.next_settlement_id = 0
     
-    def update(self):
-        pass
+    #def update(self):
+        #for s in self.settlements.values():
+            #s.update()
     
     def init_settlements(self):
         # Find the 10 largest values in the population map
         population_map = self.data["population"]
         top_10_indices = find_k_largest_value_locations(population_map, 10)
-        print(top_10_indices)
         # Initialize settlements at these locations
         for index in top_10_indices:
             self.create_settlement("placeholder", index[0], index[1])
     
 
+
     def create_settlement(self, name, r, c):
         id = self.next_settlement_id
         self.next_settlement_id += 1
-        new_settlement = Settlement(0, name, r, c)
+        new_settlement = Settlement(id, name, r, c, self.data["population"])
         self.settlements[id] = new_settlement
     
-    def get_settlement_data(self):
-        return [
-        {
-            "id": s.id,
-            "name": s.name,
-            "pos": (s.r, s.c),
-        }
-        for s in self.settlements.values()
-    ]
+    def get_all_settlements(self):
+        return self.settlements
+
+    def get_settlement_by_pos(self, pos):
+        for s in self.settlements.values():            
+            if s.r == pos[0] and s.c == pos[1]:
+                return s
+        return None
