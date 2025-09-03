1b447bd9b7026663cc17657b94ce58d5aaeed6c6
Josh France <joshfrance.476@gmail.com>
2025-09-03 21:47:16 +0100

    Added growth rate and cohesion. Linked population capacity from worldData and *5 when settlement initialised. Readded update function to increase population

--- Settlement.py
+++ Settlement.py
@@ -10,19 +10,28 @@ class Settlement:
         self.r = r
         self.c = c
         self._world_data = world_data   #Pointer to worldData. Means settlement data is linked directly to map data
-    
-    #def update(self):
-        #self.population *= 1.01
 
-    def generate_description(self):
-        prompt = f"""Task:
-        Write a short, one-sentence description of this settlement based on the context. 
-        Context: Name: {self.name}, Row: {self.r}, Col: {self.c}, Population: {self.population}, Region: {self._world_data['region'][self.r, self.c]}
-        """
-        self.description = ask_deepseek(prompt, desc_schema)
+        self.population_capacity *= 5
+
+        self.growth_rate = 1.01
+        self.cohesion = 1
+        
+    
+    def update(self):
+        self.population *= self.growth_rate
+
+    
     
     
     #Map-linked values
+    @property
+    def population_capacity(self):
+        return self._world_data["population_capacity"][self.r, self.c]
+    
+    @population_capacity.setter
+    def population_capacity(self, value):
+        self._world_data["population_capacity"][self.r, self.c] = value
+    
     @property
     def fertility(self):
         return self._world_data["fertility"][self.r, self.c]
