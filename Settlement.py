cafc0602bd2b5cca992fc3bc6a4964e8c6a5483b
Josh France <joshfrance.476@gmail.com>
2025-09-03 13:39:58 +0100

    Added 'population' property which is pointer to population in worldData

--- Settlement.py
+++ Settlement.py
@@ -1,7 +1,19 @@
 class Settlement:
 
-    def __init__(self, id, name, r, c):
+    def __init__(self, id, name, r, c, population_array):
         self.id = id
         self.name = name
         self.r = r
         self.c = c
+        self._population_array = population_array   #Pointer to population map in worldData. Means 'population' is linked to worldData
+
+    #def update(self):
+        #self.population *= 1.01
+    
+    @property
+    def population(self):
+        return self._population_array[self.r, self.c]
+    
+    @population.setter
+    def population(self, value):
+        self._population_array[self.r, self.c] = value
