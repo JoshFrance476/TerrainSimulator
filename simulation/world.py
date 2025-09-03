4bf2bd24b22b04a9084522fabe83fb4bccf85a98
Josh France <joshfrance.476@gmail.com>
2025-09-03 13:42:30 +0100

    Added functions for interacting with worldData and SettlementManager

--- simulation/world.py
+++ simulation/world.py
@@ -1,5 +1,6 @@
 from simulation.worldData import WorldData
 from SettlementManager import SettlementManager
+from utils.config import REGION_NAMES
 
 class World:
     """Handles simulator and high level world logic."""
@@ -20,8 +21,24 @@ class World:
     #def update(self):
         #self.dynamic_maps = update_dynamic_maps(self.static_maps_dict, self.dynamic_maps_dict)
     
-    def get_data_for_ui(self):
-        return self.data.get_world_data(), self.settlement_manager.get_settlement_data()
+    def get_world_data(self):
+        return self.data.get_world_data()  
+    
+    def get_region_data(self, x0, y0, x1, y1):
+        return self.data.get_region_data(x0, y0, x1, y1)
+    
+    def get_settlement_by_pos(self, pos):
+        return self.settlement_manager.get_settlement_by_pos(pos)
+
+    def get_all_settlements(self):
+        return self.settlement_manager.get_all_settlements()
+
+
+    def get_cell_data(self, selected_cell):
+        if selected_cell:
+            return self.data.get_cell_data(selected_cell), self.settlement_manager.get_settlement_by_pos(selected_cell)
+        else:
+            return None, None
 
 
     
