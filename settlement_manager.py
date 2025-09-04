3c8f5425b11b142e2be54a1c256a8315ef3b0fe0
Josh France <joshfrance.476@gmail.com>
2025-09-04 17:24:05 +0100

    Added function to find a settlements resources and pass it to new settlements

--- settlement_manager.py
+++ settlement_manager.py
@@ -1,5 +1,6 @@
 from settlement import Settlement
-from utils.config import STARTING_SETTLEMENT_COUNT
+from utils.config import STARTING_SETTLEMENT_COUNT, RESOURCE_NAMES
+import numpy as np
 
 class SettlementManager:
     def __init__(self, world, world_data):
@@ -22,9 +23,21 @@ class SettlementManager:
     def create_settlement(self, name, r, c):
         id = self.next_settlement_id
         self.next_settlement_id += 1
-        new_settlement = Settlement(id, name, r, c, self.world_data)
+
+        resources = self.return_settlement_resources(r, c)
+
+        new_settlement = Settlement(id, name, r, c, self.world_data, resources)
+
         self.settlements[id] = new_settlement
-    
+        
+    def return_settlement_resources(self, r, c):
+        resource_map = self.world.get_surrounding_data(r, c, radius=3, map="resource")
+
+        ids, counts = np.unique(resource_map, return_counts=True)
+
+        resource_counts = {RESOURCE_NAMES[rid]: int(count) for rid, count in zip(ids, counts) if rid != 0}
+
+        return resource_counts
     
     def get_all_settlements(self):
         return self.settlements
