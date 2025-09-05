d96e5fe8677e8e0a412931de8122c3832884bb16
Josh France <joshfrance.476@gmail.com>
2025-09-05 16:21:12 +0100

    Calls render_magnifier in map_renderer if magnified is true in event_handler

--- main.py
+++ main.py
@@ -39,11 +39,13 @@ while True:
         world.step(tick_count)
         tick_count += 1
     
+    screen_data = world.get_region_data(camera.x_pos, 
+                                        camera.y_pos, 
+                                        config.CAMERA_COLS+camera.x_pos, 
+                                        config.CAMERA_ROWS+camera.y_pos)
+    
     map_renderer.render_view(screen, 
-                             world.get_region_data(camera.x_pos, 
-                                                        camera.y_pos, 
-                                                        config.CAMERA_COLS+camera.x_pos, 
-                                                        config.CAMERA_ROWS+camera.y_pos), 
+                             screen_data, 
                             event_handler.get_selected_filter())
     
     ui_manager.render_ui(screen, 
@@ -53,6 +55,18 @@ while True:
                          camera.y_pos, 
                          map_renderer.get_selected_filter_name(), 
                          event_handler.get_relevant_cells())
+    
+    if event_handler.magnified:
+        selected_cell, hovered_cell = event_handler.get_relevant_cells()
+        map_renderer.render_magnifier(screen, 
+                                      world.get_region_data(hovered_cell[1]-config.MAGNIFIER_CELL_AMOUNT, 
+                                                            hovered_cell[0]-config.MAGNIFIER_CELL_AMOUNT, 
+                                                            hovered_cell[1]+config.MAGNIFIER_CELL_AMOUNT, 
+                                                            hovered_cell[0]+config.MAGNIFIER_CELL_AMOUNT),
+                                      event_handler.get_selected_filter())
+
+
+    
     
 
     pygame.display.flip()
