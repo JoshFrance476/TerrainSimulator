bf0c5fe9773c7009d4a46d4f3d4a4ac05cc8b39a
Josh France <joshfrance.476@gmail.com>
2025-09-08 11:10:30 +0100

    Added function to find eligible settlements based on population

--- simulation/settlement_manager.py
+++ simulation/settlement_manager.py
@@ -1,5 +1,5 @@
 from simulation.settlement import Settlement
-from utils.config import STARTING_SETTLEMENT_COUNT, RESOURCE_NAMES
+from utils.config import STARTING_SETTLEMENT_COUNT, RESOURCE_NAMES, SETTLEMENT_LIMIT
 from scipy.ndimage import distance_transform_edt
 import numpy as np
 
@@ -17,6 +17,8 @@ class SettlementManager:
     def update(self):
         for s in self.settlements.values():
             s.update()
+        #if len(self.settlements) < SETTLEMENT_LIMIT:
+        #    self.find_eligible_settlements()
     
 
     def create_settlement(self, name, r, c):
@@ -28,6 +30,14 @@ class SettlementManager:
         new_settlement = Settlement(id, name, r, c, self._world, resources)
 
         self.settlements[id] = new_settlement
+    
+    def find_eligible_settlements(self):
+        population_map = self._world.get_map_data("population")
+        settlement_distance_map = self._world.get_map_data("settlement_distance")
+        eligible_settlements = np.where((population_map > 3) & (settlement_distance_map > 10))
+        for r, c in zip(eligible_settlements[0], eligible_settlements[1]):
+            self.create_settlement(f"Settlement {len(self.settlements)}", r, c)
+        
     
     def create_settlement_distance_map(self, r, c):
         mask = np.ones((r, c), dtype=bool)  
