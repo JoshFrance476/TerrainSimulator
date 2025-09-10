f1de7f9de2d5e8c546bc6f8226138ea1f3fd4937
Josh France <joshfrance.476@gmail.com>
2025-09-10 11:08:48 +0100

    AppController now handles all input and world information, simplifying main loop

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from simulation.world_data import WorldData
 from simulation.world import World
 from rendering.map_renderer import MapRenderer
 from rendering.ui_manager import UIManager
-
+from app_controller import AppController
 
 
 #import tracemalloc
@@ -26,14 +26,17 @@ screen = pygame.display.set_mode((config.SCREEN_WIDTH + config.SIDEBAR_WIDTH, co
 pygame.display.set_caption("Terrain Generation")
 
 # Initialize core objects
+world = World(config.WORLD_ROWS, config.WORLD_COLS)
+
+fonts = FontManager()
+
 camera = Camera()
 event_handler = EventHandler()
-world = World(config.WORLD_ROWS, config.WORLD_COLS)
-map_renderer = MapRenderer()
-fonts = FontManager()
-ui_manager = UIManager(fonts)
 
-
+controller = AppController(world, event_handler, camera)
+ui_manager = UIManager(fonts, controller)
+map_renderer = MapRenderer(controller)
+event_handler.controller = controller
 
 clock = pygame.time.Clock()
 
@@ -42,53 +45,21 @@ tick_count = 0
 
 while True:
     events = pygame.event.get()
-    
-    event_handler.handle_events(events, camera)
 
     for event in events:
+        event_handler.handle_event(event)
         ui_manager.handle_event(event)
+    
+    event_handler.handle_continuous_inputs()
 
-    camera.clamp_pan()
-    
-    if not event_handler.paused:
-        world.step(tick_count)
-        tick_count += 1
-    
-    screen_data = world.get_region_data(camera.x_pos, 
-                                        camera.y_pos, 
-                                        config.CAMERA_COLS+camera.x_pos, 
-                                        config.CAMERA_ROWS+camera.y_pos)
-    
-    map_renderer.render_view(screen, 
-                             screen_data, 
-                            event_handler.get_selected_filter())
-    
-    
-    ui_manager.render_ui(screen, 
-                         world.get_cell_data(event_handler.selected_cell),
-                         event_handler.get_active_screens(), 
-                         world.get_all_settlements(),
-                         world.get_all_states(),
-                         camera.x_pos, 
-                         camera.y_pos, 
-                         map_renderer.get_selected_filter_name(), 
-                         event_handler.get_relevant_cells(),)
-    
-    if event_handler.magnified:
-        selected_cell, hovered_cell = event_handler.get_relevant_cells()
-        map_renderer.render_magnifier(screen, 
-                                      world.get_region_data(hovered_cell[1]-config.MAGNIFIER_CELL_AMOUNT, 
-                                                            hovered_cell[0]-config.MAGNIFIER_CELL_AMOUNT, 
-                                                            hovered_cell[1]+config.MAGNIFIER_CELL_AMOUNT, 
-                                                            hovered_cell[0]+config.MAGNIFIER_CELL_AMOUNT),
-                                      event_handler.get_selected_filter(),
-                                      event_handler.hovered_cell,
-                                      camera.x_pos,
-                                      camera.y_pos)
+    controller.update()
 
-
+    map_renderer.render_view(screen)
     
+    ui_manager.render_ui(screen, world)
     
+    if controller.magnified:
+        map_renderer.render_magnifier(screen)
 
     pygame.display.flip()
     clock.tick(60)
