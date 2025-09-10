476456d76c06918daf1f4f24125fef9b5e1532bc
Josh France <joshfrance.476@gmail.com>
2025-09-10 11:12:01 +0100

    mapRenderer now uses appController for input and world information, and map filter is referred to by name instead of ID for simplicity

--- rendering/map_renderer.py
+++ rendering/map_renderer.py
@@ -4,87 +4,75 @@ from utils.overlay_generator import apply_heatmap_overlay
 
 class MapRenderer:
     """Handles rendering the terrain and overlays on the screen."""
-
-    def __init__(self):
-        self.selected_filter_name = "None"
+    def __init__(self, controller):
+        self.controller = controller
     
-    def render_view(self, screen, view_data, map_filter):
+    def render_view(self, screen):
         '''Takes in world data and filter to produce display map, then draws to screen'''
-        display_map = self.apply_overlay(view_data, map_filter)
+        display_map = self.apply_overlay(self.controller.get_screen_data(), self.controller.selected_filter)
         self.draw_view(screen, display_map)
         
     
-    def render_magnifier(self, screen, magnifier_map, map_filter, hovered_cell, camera_x_pos, camera_y_pos):
-        magnifier_display_map = self.apply_overlay(magnifier_map, map_filter)
-        self.draw_magnifier(screen, magnifier_display_map, hovered_cell, camera_x_pos, camera_y_pos)
+    def render_magnifier(self, screen):
+        magnifier_display_map = self.apply_overlay(self.controller.get_magnifier_data(), self.controller.selected_filter)
+        self.draw_magnifier(screen, magnifier_display_map, self.controller.hovered_cell, self.controller.get_camera_position())
 
     
     def apply_overlay(self, view_data, map_filter):
         '''Applies selected overlay to the base colour map.'''
-        if map_filter == 0:
+        if map_filter == "None":
             display_map = view_data["colour"]
-            self.selected_filter_name = "None"
-        elif map_filter == 1:
+        elif map_filter == "Elevation":
             display_map = apply_heatmap_overlay(
                     view_data["coastline"],
                     view_data["elevation"]
                 )
-            self.selected_filter_name = "Elevation"
-        elif map_filter == 2:
+        elif map_filter == "Temperature":
             display_map = apply_heatmap_overlay(
                     view_data["coastline"],
                     view_data["temperature"]
                 )
-            self.selected_filter_name = "Temperature"
-        elif map_filter == 3:
+        elif map_filter == "Rainfall":
             display_map = apply_heatmap_overlay(
                     view_data["coastline"],
                     view_data["rainfall"]
                 )
-            self.selected_filter_name = "Rainfall"
-        elif map_filter == 4:
+        elif map_filter == "Population Capacity":
             display_map = apply_heatmap_overlay(
                     view_data["coastline"],
                     view_data["population_capacity"]
                 )
-            self.selected_filter_name = "Population Capacity"
-        elif map_filter == 5:
+        elif map_filter == "Fertility":
             display_map = apply_heatmap_overlay(
                     view_data["coastline"],
                     view_data["fertility"]
                 )
-            self.selected_filter_name = "Fertility"
-        elif map_filter == 6:
+        elif map_filter == "Traversal Cost":
             display_map = apply_heatmap_overlay(
                     view_data["coastline"],
                     view_data["traversal_cost"]
                 )
-            self.selected_filter_name = "Traversal Cost"
-        elif map_filter == 7:
+        elif map_filter == "Steepness":
             display_map = apply_heatmap_overlay(
                     view_data["coastline"],
                     view_data["steepness"]
                 )
-            self.selected_filter_name = "Steepness"
-        elif map_filter == 8:
+        elif map_filter == "Population":
             display_map = apply_heatmap_overlay(
                     view_data["coastline"],
                     view_data["population"]
                 )
-            self.selected_filter_name = "Population"
-        elif map_filter == 9:
+        elif map_filter == "Resource":
             display_map = view_data["colour"].copy()
             resource_map = view_data["resource"]
             for rid, color in config.RESOURCE_COLORS.items():
                 display_map[resource_map == rid] = color
-            self.selected_filter_name = "Resource"
-        elif map_filter == 10:
+        elif map_filter == "State":
             display_map = view_data["colour"].copy()
             state_map = view_data["state"]
             for state_id, color in config.STATE_COLOURS.items():
                 state_map_mask = state_map != 255
                 display_map[state_map_mask & (state_map % len(config.STATE_COLOURS) == state_id)] = color
-            self.selected_filter_name = "State"
         return display_map
 
 
@@ -95,12 +83,10 @@ class MapRenderer:
                                                 display_map.shape[0] * config.CELL_SIZE))
         screen.blit(surface, (config.SIDEBAR_WIDTH, 0))
     
-    def draw_magnifier(self, screen, magnifier_map, hovered_cell, camera_x_pos, camera_y_pos):
+    def draw_magnifier(self, screen, magnifier_map, hovered_cell, camera_position):
         magnifier_surface = pygame.surfarray.make_surface(magnifier_map.swapaxes(0, 1))
         magnifier_surface = pygame.transform.scale(magnifier_surface, (magnifier_map.shape[1] * config.MAGNIFIER_CELL_SIZE,
                                                                           magnifier_map.shape[0] * config.MAGNIFIER_CELL_SIZE))
-        screen.blit(magnifier_surface, ((hovered_cell[1]*config.CELL_SIZE)+config.SIDEBAR_WIDTH-(camera_x_pos*config.CELL_SIZE) - (config.MAGNIFIER_CELL_SIZE*magnifier_map.shape[1]//2),
-                                        (hovered_cell[0]*config.CELL_SIZE)-camera_y_pos*config.CELL_SIZE - (config.MAGNIFIER_CELL_SIZE*magnifier_map.shape[0]//2)))
+        screen.blit(magnifier_surface, ((hovered_cell[1]*config.CELL_SIZE)+config.SIDEBAR_WIDTH-(camera_position[0]*config.CELL_SIZE) - (config.MAGNIFIER_CELL_SIZE*magnifier_map.shape[1]//2),
+                                        (hovered_cell[0]*config.CELL_SIZE)-camera_position[1]*config.CELL_SIZE - (config.MAGNIFIER_CELL_SIZE*magnifier_map.shape[0]//2)))
 
-    def get_selected_filter_name(self):
-        return self.selected_filter_name
