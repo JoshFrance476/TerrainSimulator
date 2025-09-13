aae0cd9112bdb642a983dcd7b3ca17e7405f46cb
Josh France <joshfrance.476@gmail.com>
2025-09-13 19:32:42 +0100

    Initial commit. Contains 'wrap_text' function to prevent text overflow in UI

---
+++ utils/ui_utils.py
@@ -0,0 +1,13 @@
+def wrap_text(text, font, max_width):
+    words = text.split(" ")
+    lines, current = [], ""
+    for word in words:
+        test = current + (" " if current else "") + word
+        if font.size(test)[0] <= max_width:
+            current = test
+        else:
+            lines.append(current)
+            current = word
+    if current:
+        lines.append(current)
+    return lines
