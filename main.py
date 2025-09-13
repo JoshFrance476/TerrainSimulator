086241a5b7d4d2cbc43a0cbf7ac94e658618dfc5
Josh France <joshfrance.476@gmail.com>
2025-09-13 14:07:54 +0100

    Renamed event_handler to input_handler to avoid confusion with event_manager

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pygame
 import utils.config as config
 from camera import Camera
-from input_handler import EventHandler
+from input_handler import InputHandler
 from simulation.world_data import WorldData
 from simulation.world import World
 from rendering.map_renderer import MapRenderer
 from rendering.ui_manager import UIManager
 from app_controller import AppController
-
 
 #import tracemalloc
 #tracemalloc.start()
@@ -27,7 +26,6 @@ pygame.display.set_caption("Terrain Generation")
 
 # Initialize core objects
 world = World(config.WORLD_ROWS, config.WORLD_COLS)
-
 fonts = FontManager()
 
 camera = Camera()
@@ -35,7 +33,7 @@ camera = Camera()
 controller = AppController(world, camera)
 ui_manager = UIManager(fonts, controller)
 map_renderer = MapRenderer(controller)
-event_handler = EventHandler(controller)
+input_handler = InputHandler(controller)
 
 clock = pygame.time.Clock()
 
@@ -46,10 +44,10 @@ while True:
     events = pygame.event.get()
 
     for event in events:
-        event_handler.handle_event(event)
+        input_handler.handle_event(event)
         ui_manager.handle_event(event)
     
-    event_handler.handle_continuous_inputs()
+    input_handler.handle_continuous_inputs()
 
     controller.update()
 
