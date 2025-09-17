5f3d2e2fbdb4690868609812d5782d558c932e71
Josh France <joshfrance.476@gmail.com>
2025-09-17 17:30:58 +0100

    Moved API key to .env file and imported

--- utils/llm_utils.py
+++ utils/llm_utils.py
@@ -1,5 +1,8 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()  # loads .env into os.environ
 import os
+ 
 client = OpenAI(
     api_key= os.getenv("DEEPSEEK_API_KEY"),
     base_url="https://api.deepseek.com"
