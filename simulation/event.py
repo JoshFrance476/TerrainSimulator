728fc4009b601d10c689a11b44d9b24d79c0463b
Josh France <joshfrance.476@gmail.com>
2025-10-16 16:16:12 +0100

    Added descriptions and narratives to events

--- simulation/event.py
+++ simulation/event.py
@@ -1,5 +1,10 @@
 class Event:
-    def __init__(self, event_type, location, tick_count):
+    def __init__(self, event_type, location, tick_count, description):
         self.event_type = event_type
         self.location = location
         self.tick_count = tick_count
+        self.description = description
+        self.narrative = ""
+    
+    def add_event_narrative(self, narrative):
+        self.narrative = narrative
