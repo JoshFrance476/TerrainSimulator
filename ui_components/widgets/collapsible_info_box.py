6d088d60524745b47a30a54cb19a557b1813ca38
Josh France <joshfrance.476@gmail.com>
2025-10-24 20:52:18 +0100

    Added handling for embedded info_box_list

--- ui_components/widgets/collapsible_info_box.py
+++ ui_components/widgets/collapsible_info_box.py
@@ -14,14 +14,16 @@ class CollapsibleInfoBox(InfoBox):
         self.expanded = not self.expanded
         self.update_height()
 
-    def draw(self, screen, x, y):
-        super().draw(screen, x, y)
+    def draw(self, screen, x, y, parent_clip=None):
+        super().draw(screen, x, y, parent_clip)
         self.toggle_button.rect.topleft = (x + self.width - self.toggle_button.rect.width - 4, y + 4)
         self.toggle_button.draw(screen)
 
     def handle_event(self, event):
         self.toggle_button.handle_event(event)
         self.title.handle_event(event)
+        if self.info_box_list:
+            self.info_box_list.handle_event(event)
 
 
 
