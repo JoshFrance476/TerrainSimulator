0f5f954d8adc4c9364f1cfcba73b511e452cdda9
Josh France <joshfrance.476@gmail.com>
2025-10-26 15:35:30 +0000

    Improved world labelling system but still needs work

--- generation/generator_main.py
+++ generation/generator_main.py
@@ -3,14 +3,14 @@ from generation.stage_2_generator import generate_stage_2
 from generation.stage_3_generator import generate_stage_3
 from generation.stage_4_generator import generate_stage_4
 from utils.colour_utils import generate_color_map
-from utils.map_utils import produce_landmass_label_map, produce_label_dict, produce_continent_label_map, produce_ocean_label_map, produce_water_body_label_map
+from utils.map_utils import produce_landmass_label_map, produce_continent_label_map, produce_ocean_label_map, produce_water_body_label_map
 
 import numpy as np
 import time
 import logging
 import config as config
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARNING)
 
 
 def generate_data_maps(rows, cols):
@@ -30,25 +30,34 @@ def generate_data_maps(rows, cols):
     population_capacity_map, population_map, resource_map = generate_stage_4(fertility_map, temperature_map, river_proximity_map, sea_map, river_map, elevation_map, region_map, rainfall_map)
     logging.debug(f"Stage 4 generation took {time.time() - start_time:.2f} seconds")
 
-
+    start_time = time.time()
     colour_map = generate_color_map({
         'elevation': elevation_map,
         'region': region_map,
         'steepness': steepness_map
     }, True, True)
+    logging.debug(f"Colour map generation took {time.time() - start_time:.2f} seconds")
 
-    water_body_label_map = produce_water_body_label_map(sea_map)
+    land_map = ~sea_map
 
-    ocean_label_map = produce_ocean_label_map(water_body_label_map)
+    start_time = time.time()
+    water_body_label_map, water_body_dict = produce_water_body_label_map(sea_map)  #water body map labels every separate water body
 
-    landmass_label_map = produce_landmass_label_map(~sea_map, ocean_label_map)
+    ocean_label_map, ocean_dict = produce_ocean_label_map(water_body_label_map, threshold=200)  #ocean map labels all water bodies larger than threshold
 
-    continent_label_map, test_map = produce_continent_label_map(landmass_label_map)
+    landmass_label_map, landmass_dict = produce_landmass_label_map(land_map, ocean_label_map)  #landmass map labels every separate landmass, including lakes (water bodies below ocean threshold)
+
+    #continent map labels each section of landmass that is separated by less than (threshold * 2) - 1 cells to the rest of the landmass (ignoring lakes)
+    continent_label_map, continent_dict = produce_continent_label_map(landmass_label_map, threshold=4)  
+
     land_feature_label_map = np.zeros_like(sea_map)
 
-
+    land_feature_dict = {}
+    water_feature_dict = {}
 
     water_feature_label_map = np.zeros_like(sea_map)
+
+    logging.debug(f"Label map generation took {time.time() - start_time:.2f} seconds")
 
     world_data = {
         'colour': colour_map,
@@ -84,34 +93,25 @@ def generate_data_maps(rows, cols):
         #label maps
         'landmass_label': landmass_label_map,
         'continent_label': continent_label_map,
-        'land_feature_label': test_map,
+        'land_feature_label': land_feature_label_map,
         'water_body_label': water_body_label_map,
         'ocean_label': ocean_label_map,
         'water_feature_label': water_feature_label_map
         }
+    
+    label_lookups = {
+        'landmass': landmass_dict,
+        'continent': continent_dict,
+        'land_feature': land_feature_dict,
+        'water_body': water_body_dict,
+        'ocean': ocean_dict,
+        'water_feature': water_feature_dict
+    }
 
     
-    return world_data
-
-def generate_label_lookups(landmass_label_map, continent_label_map, land_feature_label_map, water_body_label_map, ocean_label_map, water_feature_label_map):
-    landmass_label_lookup = produce_label_dict(landmass_label_map)
-    continent_label_lookup = {}
-    land_feature_label_lookup = {}
-
-    water_body_label_lookup = produce_label_dict(water_body_label_map)
-    ocean_label_lookup = {}
-    water_feature_label_lookup = {}
+    return world_data, label_lookups
 
 
-    label_lookups = {
-        'landmass': landmass_label_lookup,
-        'continent': continent_label_lookup,
-        'land_feature': land_feature_label_lookup,
-        'water_body': water_body_label_lookup,
-        'ocean': ocean_label_lookup,
-        'water_feature': water_feature_label_lookup
-    }
-    return label_lookups
 
 
 
