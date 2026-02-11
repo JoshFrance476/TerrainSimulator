27bd08eb89f0519f57b7851ca848e064c24019bb
Josh France <joshfrance.476@gmail.com>
2026-02-11 16:33:46 +0000

    Removed obsolete files

--- simulation/state.py
+++
@@ -1,6 +0,0 @@
-class State:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-        self.tile_capacity = 1
-        self.tile_count = 0
--- simulation/storyline.py
+++
@@ -1,9 +0,0 @@
-class Storyline:
-    def __init__(self, overview, location, scope):
-        self.overview = overview
-        self.location = location
-        self.scope = scope
-        self.events = []
-    
-    def add_event(self, event):
-        self.events.append(event)
