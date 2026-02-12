4954f99c695b0488259bbef61b54b07d0f6c3ff8
Josh France <joshfrance.476@gmail.com>
2026-02-12 19:47:34 +0000

    Introduces basic version of regions

---
+++ simulation/region.py
@@ -0,0 +1,7 @@
+class Region:
+    def __init__(self, rid):
+        self.rid = rid
+        self.title = ""
+        self.visible_desc = ""
+        self.hidden_desc = ""
+        
---
+++ simulation/region_manager.py
@@ -0,0 +1,39 @@
+from simulation.region import Region
+import numpy as np
+from config import WORLD_COLS, WORLD_ROWS
+
+class RegionManager:
+    def __init__(self):
+        self.region_map = [[[] for x in range(WORLD_COLS)] for y in range(WORLD_ROWS)] 
+        self.region_list = []
+        self.rid_counter = 0
+
+    
+    def create_region(self, location):
+        new_region = Region(self.rid_counter)
+        self.rid_counter += 1
+        self.add_region_to_location(location, new_region.rid)
+        self.region_list.append(new_region)
+        return new_region.rid
+
+    def add_region_to_location(self, location, rid):
+        grid_address = self.region_map[location[0]][location[1]]
+        if rid not in grid_address:
+            grid_address.append(rid)
+
+
+
+    def get_values(self, r, c):
+        cell = self.region_map[r][c]
+        return cell
+    
+    def get_region_map(self):
+        return self.region_map
+    
+    def get_regions_at_location(self, location):
+        region_list = []
+        grid_address = self.region_map[location[0]][location[1]]
+        for rid in grid_address:
+            region = self.region_list[rid]
+            region_list.append(region)
+        return region_list
