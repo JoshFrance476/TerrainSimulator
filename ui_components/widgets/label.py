c992d471f7317f1d22695e419e51421d8ce53755
Josh France <joshfrance.476@gmail.com>
2026-02-13 15:22:18 +0000

    Label now wraps text and dynamically calculated width and height

--- ui_components/widgets/label.py
+++ ui_components/widgets/label.py
@@ -1,11 +1,22 @@
-import pygame
+from utils.ui_utils import wrap_text
 
 class Label:
-    def __init__(self, text, small_font, height):
+    def __init__(self, text, font, max_width):
         self.text = text
-        self.small_font = small_font
-        self.height = height
+        self.font = font
+        self.max_width = max_width
+        self.width = 0
+        self.wrapped_text = wrap_text(self.text, font, max_width)
+        self.height = 25 + (15 * max(0,(len(self.wrapped_text)-1)))
+
+        for line in self.wrapped_text:
+            if self.width < font.size(line)[0]:
+                self.width = font.size(line)[0]
     
     def draw(self, screen, x, y):
-        text_surface = self.small_font.render(self.text, True, (30,30,30))
-        screen.blit(text_surface, (x, y))
+        y_offset = y
+
+        for line in self.wrapped_text:
+            text_surface = self.font.render(line, True, (30,30,30))
+            screen.blit(text_surface, (x, y_offset))
+            y_offset += 15
