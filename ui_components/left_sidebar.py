4e9ebf49872269cc60631348d7153451e5923450
Josh France <joshfrance.476@gmail.com>
2026-02-13 11:52:45 +0000

    Left sidebar now functions as a dynamic list of components

--- ui_components/left_sidebar.py
+++ ui_components/left_sidebar.py
@@ -1,14 +1,20 @@
 import pygame
 import config
-from ui_components.widgets.info_box_list import InfoBoxList
+from ui_components.widgets.textbox import TextBox
 
 class LeftSidebarController:
     def __init__(self, fonts, controller):
         self.fonts = fonts
         self.controller = controller
         self.title = ""
-        self.info_box_list = InfoBoxList(config.SIDEBAR_WIDTH - 20, config.SCREEN_HEIGHT-60)
+        self.component_list = []
 
+        self.show_add_region_page()
+
+    def show_add_region_page(self):
+        self.component_list = []
+        self.component_list.append(TextBox(self.controller, self.fonts.small_font, 220, 25))
+        self.component_list.append(TextBox(self.controller, self.fonts.small_font, 220, 25))
 
     def draw(self, screen):
         pygame.draw.rect(screen, (220,220,220),
@@ -19,8 +25,12 @@ class LeftSidebarController:
         title_text = self.fonts.large_font.render(self.title, True, (30,30,30))
         screen.blit(title_text, (10, 20))
 
-        if self.info_box_list:
-            self.info_box_list.draw(screen, 10, 50)
+        y_offset = 10
+        for component in self.component_list:
+            if isinstance(component, TextBox):
+                component.draw(screen, 10, y_offset)
+            y_offset += 30
+
 
 
 
