e848cbacc55abbfaa3e4b3ea739a01dfd1ac0cdc
Josh France <joshfrance.476@gmail.com>
2026-02-14 14:19:59 +0000

    Added biome configuration files

---
+++ utils/biome_config_manager.py
@@ -0,0 +1,50 @@
+import json
+import numpy as np
+import colorsys
+
+class BiomeConfigManager:
+    def __init__(self):
+        self.config = {}
+        self.name_to_id = {}
+        self.by_name = {}
+        self.colour_lookup = {}
+        self.cost_lookup = {}
+    
+    def load_biome_config_file(self, file_name):
+        with open(file_name, "r") as f:
+            biome_config_file = json.load(f)
+            
+        BIOME_CONSTANTS = biome_config_file.get("constants", {})
+        BIOME_RULES = biome_config_file["biomes"]
+
+        self.config = self.resolve_constants(BIOME_RULES, BIOME_CONSTANTS)
+
+        self.name_to_id = {r["name"]: idx for idx, r in enumerate(self.config)}
+
+        self.by_name = {r["name"]: r for r in self.config}
+
+        self.colour_lookup = np.asarray(
+            [(r["colour"]["h"], r["colour"]["s"], r["colour"]["v"]) for r in self.config],
+            dtype=np.float32
+        )  # shape (N, 3)
+
+
+
+        self.cost_lookup = np.array(
+            [r["base_traversal_cost"] for r in self.config],
+            dtype=np.float32
+)
+
+    
+    def resolve_constants(self, obj, constants):
+        if isinstance(obj, dict):
+            return {
+                k: self.resolve_constants(
+                    constants.get(v, v) if isinstance(v, str) else v,
+                    constants
+                )
+                for k, v in obj.items()
+            }
+        if isinstance(obj, list):
+            return [self.resolve_constants(v, constants) for v in obj]
+        return obj
