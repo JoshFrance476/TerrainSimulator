ffd75b4c460efda0f61b74c7b1c8ffca2f293b1b
Josh France <joshfrance.476@gmail.com>
2026-02-14 12:03:04 +0000

    Added slider text, better configuration and keyboard incrementing

--- ui_components/widgets/slider.py
+++ ui_components/widgets/slider.py
@@ -1,36 +1,51 @@
 import pygame
 
 class Slider:
-    def __init__(self, min_val, max_val, width):
+    def __init__(self, font, min_val, max_val, bar_width, default_value = -1):
         self.min = min_val
         self.max = max_val
-        self.width = width
+        self.bar_width = bar_width
         self.height = 10
-        self.value = min_val
-        self.padding = 30
+        self.left_padding = 10
+        self.font = font
+        self.bar_x_offset = 60
 
+        if default_value >= min_val and default_value <= max_val:
+            self.value = default_value
+        else:
+            self.value = min_val
+
+        self.dragging = False
         self.focused = False
 
         self.bar_rect = None
         self.pin_rect = None
 
     def draw(self, screen, x, y):
-        self.bar_rect = pygame.Rect(x, y, self.width, self.height)
+        self.bar_rect = pygame.Rect(x+self.bar_x_offset, y, self.bar_width, self.height)
 
         # value → pixel
         t = (self.value - self.min) / (self.max - self.min)
-        pin_x = x + t * self.width
+        pin_x = x + t * self.bar_width
 
-        self.pin_rect = pygame.Rect(pin_x - 10, y - 5, 20, 20)
+        self.pin_rect = pygame.Rect(pin_x + self.bar_x_offset-10, y - 5, 20, 20)
 
         pygame.draw.rect(screen, (0, 0, 0), self.bar_rect, 2)
         pygame.draw.rect(screen, (0, 0, 0), self.pin_rect)
 
+        value_text = self.font.render(str(self.value), True, (30,30,30))
+        screen.blit(value_text, (x, y-3))
+
+        min_text = self.font.render(str(self.min), True, (30,30,30))
+        screen.blit(min_text, (x+self.bar_x_offset-20, y-3))
+
+        max_text = self.font.render(str(self.max), True, (30,30,30))
+        screen.blit(max_text, (x+self.bar_width+self.bar_x_offset+10, y-3))
+
     def collide_with(self, mouse_pos):
         return self.bar_rect and self.bar_rect.collidepoint(mouse_pos)
 
-    def is_clicked(self, event):
-        mouse_x = event.pos[0]
+    def update_value(self, mouse_x):
         if not self.bar_rect:
             return
 
@@ -39,9 +54,26 @@ class Slider:
                       min(mouse_x, self.bar_rect.right))
 
         # pixel → value
-        t = (mouse_x - self.bar_rect.left) / self.width
+        t = (mouse_x - self.bar_rect.left) / self.bar_width
         self.value = int(self.min + t * (self.max - self.min))
-        print(self.value)
+
+    def is_clicked(self, event):
+        self.dragging = True
+        self.update_value(event.pos[0])
+        
     
     def is_dragged(self, event):
-        self.is_clicked(event)
+        if self.dragging:
+            self.update_value(event.pos[0])
+
+    def stop_drag(self):
+        self.dragging = False
+    
+    def increment(self):
+        self.value += 1
+
+    def decrement(self):
+        self.value -= 1
+    
+
+    
