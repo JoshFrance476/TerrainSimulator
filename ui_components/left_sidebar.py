4fcb19289dd9695453c81653ab17e274d71ca83a
Josh France <joshfrance.476@gmail.com>
2026-02-14 14:21:36 +0000

    Added tile setup page

--- ui_components/left_sidebar.py
+++ ui_components/left_sidebar.py
@@ -4,12 +4,14 @@ from ui_components.widgets.textbox import TextBox
 from ui_components.widgets.label import Label
 from ui_components.widgets.button import Button
 from ui_components.widgets.slider import Slider
+from ui_components.widgets.colour_preview import ColourPreview
 
 class LeftSidebarController:
-    def __init__(self, fonts, controller):
+    def __init__(self, fonts, controller, biome_config):
         self.fonts = fonts
         self.controller = controller
         self.component_list = []
+        self.biome_config = biome_config
 
 
     def show_region_setup_page(self):
@@ -24,7 +26,36 @@ class LeftSidebarController:
         self.component_list.append(Button(50, 25, lambda: self.controller.set_painted_region_info(self.component_list[2].text, self.component_list[4].text, self.component_list[6].text)))
     
     def show_tile_setup_page(self):
-        self.component_list.append(Slider(0, 100, config.SIDEBAR_WIDTH-60))
+        self.component_list.append(
+            Label("Tile Setup", self.fonts.large_font, config.SIDEBAR_WIDTH)
+        )
+
+        hue_slider = Slider(self.fonts.small_font, 0, 360, config.SIDEBAR_WIDTH - 120)
+        sat_slider = Slider(self.fonts.small_font, 0, 100, config.SIDEBAR_WIDTH - 120, 100)
+        val_slider = Slider(self.fonts.small_font, 0, 100, config.SIDEBAR_WIDTH - 120, 100)
+
+        self.component_list.append(ColourPreview(50, 50, hue_slider, sat_slider, val_slider))
+
+        self.component_list.append(Label("Tile name:", self.fonts.large_font, config.SIDEBAR_WIDTH))
+        self.component_list.append(TextBox(self.controller, self.fonts.small_font, 150, 20))
+
+        self.component_list.append(Label("Traversal Cost:", self.fonts.large_font, config.SIDEBAR_WIDTH))
+        self.component_list.append(TextBox(self.controller, self.fonts.small_font, 150, 20))
+
+        self.component_list.append(Label("Colour:", self.fonts.large_font, config.SIDEBAR_WIDTH))
+
+        self.component_list.append(Label("Hue:", self.fonts.small_font, config.SIDEBAR_WIDTH))
+        self.component_list.append(hue_slider)
+
+        self.component_list.append(Label("Saturation:", self.fonts.small_font, config.SIDEBAR_WIDTH))
+        self.component_list.append(sat_slider)
+
+        self.component_list.append(Label("Value:", self.fonts.small_font, config.SIDEBAR_WIDTH))
+        self.component_list.append(val_slider)
+
+        
+    def show_tile_manager_page(self):
+        self.component_list.append(Label("Tile Manager", self.fonts.large_font, config.SIDEBAR_WIDTH))
 
     def clear_page(self):
         self.component_list = []
@@ -37,8 +68,15 @@ class LeftSidebarController:
 
         y_offset = 10
         for component in self.component_list:
-            component.draw(screen, component.padding, y_offset)
-            y_offset += component.height
+            if isinstance(component, list):
+                x_offset = 0
+                for subcomponent in component:
+                    subcomponent.draw(screen, subcomponent.left_padding+x_offset, y_offset)
+                    x_offset += subcomponent.width
+                y_offset += component[0].height
+            else:
+                component.draw(screen, component.left_padding, y_offset)
+                y_offset += component.height
 
 
 
