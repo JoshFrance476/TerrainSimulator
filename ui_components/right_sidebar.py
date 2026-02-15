49289e6dbe549f0dfbe595fd5033da0e7a14273c
Josh France <joshfrance.476@gmail.com>
2026-02-15 20:35:08 +0000

    Update right sidebar to modular system with a location info page

--- ui_components/right_sidebar.py
+++ ui_components/right_sidebar.py
@@ -1,64 +1,39 @@
 import pygame
 import config
+from ui_components.widgets.label import Label
+from ui_components.widgets.line_divider import LineDivider
+
 
 class RightSidebarController:
     def __init__(self, fonts, controller, biome_config):
         self.fonts = fonts
         self.controller = controller
-        self.title = ""
-        self.info_list = {}
-        self.buttons = []
-        self.textbox = None
+        self.component_list = []
         self.biome_config = biome_config
+
     
 
-    def show_cell_info(self, cell_data):
-        self.buttons = []
-        self.textbox = None
-        self.title = "Cell Info"
-        if cell_data:
-            self.info_list = {
-                "Row": self.controller.get_selected_cell()[0],
-                "Col": self.controller.get_selected_cell()[1],
-                "Biome": self.biome_config.config[cell_data["biome"]]["name"].title(),
-                "Elevation": f"{cell_data['elevation']:.2f}",
-                "Temperature": f"{cell_data['temperature']:.2f}",
-                "Rainfall": f"{cell_data['rainfall']:.2f}",
-                "Steepness": f"{cell_data['steepness']:.2f}",
-                "Traversal Cost": f"{cell_data['traversal_cost']:.2f}",
-                "Colour": tuple(round(float(x),2) for x in cell_data["colour"]),
-                "Sea Proximity": cell_data['sea_proximity']
-            }
-        else:
-            self.info_list = {}
+    def show_location_info_page(self):
+        self.component_list = []
+        self.component_list.append(Label("Location", self.fonts.large_font, config.SIDEBAR_WIDTH))
+        self.component_list.append(Label(self.biome_config.config[self.controller.get_biome_at(self.controller.get_selected_cell())]["name"].title(), self.fonts.small_font, config.SIDEBAR_WIDTH))
+        self.component_list.append(LineDivider(config.SIDEBAR_WIDTH-20, 2))
     
         
-    def draw(self, screen, filter_name):
-        # Draw sidebar background
-        pygame.draw.rect(screen, (220, 220, 220), (config.SCREEN_WIDTH, 0, config.SIDEBAR_WIDTH, config.SCREEN_HEIGHT))  
+    def draw(self, screen):
+        pygame.draw.rect(screen, (220,220,220),
+                         (config.SCREEN_WIDTH, 0, config.SIDEBAR_WIDTH, config.SCREEN_HEIGHT))
+        pygame.draw.rect(screen, (80,80,80),
+                         (config.SCREEN_WIDTH, 0, config.SIDEBAR_WIDTH, config.SCREEN_HEIGHT), 3)
 
-        # Draw sidebar border (Black, 3px thickness)
-        pygame.draw.rect(screen, (80, 80, 80), (config.SCREEN_WIDTH, 0, config.SIDEBAR_WIDTH, config.SCREEN_HEIGHT), 3)
-
-
-        title_text = self.fonts.large_font.render(self.title, True, (30, 30, 30))
-        screen.blit(title_text, (config.SCREEN_WIDTH + 10, 20))
-        
-        for i, (label, value) in enumerate(self.info_list.items()):
-            text_surface = self.fonts.small_font.render(f"{label}: {value}", True, (30, 30, 30))
-            screen.blit(text_surface, (config.SCREEN_WIDTH + 10, 50 + i * 20))
-        
-        for button in self.buttons:
-            button.draw(screen)
-        
-        if self.textbox:
-            self.textbox.draw(screen)
-        
-        filter_text = self.fonts.small_font.render(f"Filter: {filter_name}", True, (30, 30, 30))
-        screen.blit(filter_text, (config.SCREEN_WIDTH + 10, config.SCREEN_HEIGHT - 40))
-    
-    def handle_event(self, event):
-        for b in self.buttons:
-            b.handle_event(event)
-        if self.textbox:
-            self.textbox.handle_mouse_input(event)
+        y_offset = 10
+        for component in self.component_list:
+            if isinstance(component, list):
+                x_offset = config.SCREEN_WIDTH
+                for subcomponent in component:
+                    subcomponent.draw(screen, subcomponent.left_padding+x_offset, y_offset)
+                    x_offset += subcomponent.width
+                y_offset += component[0].height
+            else:
+                component.draw(screen, config.SCREEN_WIDTH+component.left_padding, y_offset)
+                y_offset += component.height
