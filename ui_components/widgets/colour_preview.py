2058d167e812e73ff3b7587f787124e108fc074a
Josh France <joshfrance.476@gmail.com>
2026-02-15 10:56:04 +0000

    ColourPreview now handles different types of input - from sliders or raw input

--- ui_components/widgets/colour_preview.py
+++ ui_components/widgets/colour_preview.py
@@ -2,17 +2,41 @@ import pygame
 from utils.colour_utils import hsv2rgb
 
 class ColourPreview:
+    """
+    Accepts HSV colours in the format h[0-360], s[0-1], v[0-1]
+    """
     def __init__(self, width, height, h, s, v):
         self.width = width
         self.height = height
-        self.h_slider, self.s_slider, self.v_slider = h, s, v
-
         self.left_padding = 5
+
+        self.h_src = h
+        self.s_src = s
+        self.v_src = v
+
+    @staticmethod
+    def _get(src, normalised = False):
+        # Slider-like object
+        if hasattr(src, "value"):
+            if normalised:
+                return float(src.get_normalised_value())
+            else:
+                return float(src.value)
+        # Callable
+        if callable(src):
+            return float(src())
+        # Raw value
+        return float(src)
     
+
     def draw(self, screen, x, y):
-        rgb_colour = hsv2rgb(self.h_slider.value/360, self.s_slider.value/100, self.v_slider.value/100)
-        pygame.draw.rect(screen, (0,0,0), (x, y, self.width, self.height))
-        pygame.draw.rect(screen, rgb_colour, (x+2, y+2, self.width-4, self.height-4))
+        h = self._get(self.h_src)
+        s = self._get(self.s_src, True)
+        v = self._get(self.v_src, True)
+
+        r, g, b = hsv2rgb(h, s, v)
+        
 
 
-    
+        pygame.draw.rect(screen, (0, 0, 0), (x, y, self.width, self.height))
+        pygame.draw.rect(screen, (r, g ,b), (x + 2, y + 2, self.width - 4, self.height - 4))
