f23cc2731d2fd912bdc3db0f28845295a207b577
Josh France <joshfrance.476@gmail.com>
2026-02-15 17:35:23 +0000

    Add 'focused' attribute that can be enabled to highlight when button is toggled

--- ui_components/widgets/button.py
+++ ui_components/widgets/button.py
@@ -1,26 +1,27 @@
 import pygame
 
 class Button:
-    def __init__(self,width, height, action, label="", font=None, toggle=False):
+    def __init__(self,width, height, action, label="", font=None):
         self.rect = None
         self.action = action
         self.label = label
         self.font = font
-        self.toggle = toggle
-        self.toggled = False
         self.width = width
         self.height = height
         self.left_padding = 10
+        
+        self.focused = False
 
     def draw(self, screen, x, y):
         self.rect = pygame.Rect(x, y, self.width, self.height)
 
-        if self.toggle and self.toggled:
+        if self.focused:
             base_color = (150, 150, 220)  # toggled on
         elif self.collide_with(pygame.mouse.get_pos()):
             base_color = (180, 180, 180)  # hover
         else:
             base_color = (220, 220, 220)  # normal
+  
 
         pygame.draw.rect(screen, base_color, self.rect)
         pygame.draw.rect(screen, (80, 80, 80), self.rect, 2)
@@ -31,8 +32,6 @@ class Button:
             screen.blit(text, text_rect)
 
     def is_clicked(self, event):
-        if self.toggle:
-            self.toggled = not self.toggled
         self.action()
     
     def collide_with(self, mouse_pos):
