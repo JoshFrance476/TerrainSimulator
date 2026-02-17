b4fdbc69e2b05cc75cd6efe7fba2f6d9e48f6958
Josh France <joshfrance.476@gmail.com>
2026-02-17 10:17:21 +0000

    Add top and bottom padding attributes to line divider, making it a flexible component to adjust spacing between vertical components

--- ui_components/widgets/line_divider.py
+++ ui_components/widgets/line_divider.py
@@ -1,11 +1,13 @@
 import pygame
 
 class LineDivider:
-    def __init__(self, width, thickness=2):
+    def __init__(self, width, thickness=2, top_padding = 0, bottom_padding = 0):
         self.width = width
         self.left_padding = 10
-        self.height = 5
         self.thickness = thickness
+        self.top_padding = top_padding
+        self.bottom_padding = bottom_padding
+        self.height = self.top_padding + self.bottom_padding
     
     def draw(self, screen, x, y):
-        pygame.draw.line(screen, (0,0,0), (x, y), (x+self.width, y), self.thickness)
+        pygame.draw.line(screen, (0,0,0), (x, y+self.top_padding), (x+self.width, y+self.top_padding), self.thickness)
