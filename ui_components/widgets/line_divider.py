979cb11c871e1a779e2a442e5892e22ba64df5e9
Josh France <joshfrance.476@gmail.com>
2026-02-19 14:46:34 +0000

    Tweak lineDivider to render in the center of given width with side_padding on both sides

--- ui_components/widgets/line_divider.py
+++ ui_components/widgets/line_divider.py
@@ -1,13 +1,13 @@
 import pygame
 
 class LineDivider:
-    def __init__(self, width, thickness=2, top_padding = 0, bottom_padding = 0):
+    def __init__(self, width, thickness=2, side_padding = 10, top_padding = 0, bottom_padding = 0):
         self.width = width
-        self.left_padding = 10
+        self.left_padding = side_padding
         self.thickness = thickness
         self.top_padding = top_padding
         self.bottom_padding = bottom_padding
         self.height = self.top_padding + self.bottom_padding
     
     def draw(self, screen, x, y):
-        pygame.draw.line(screen, (0,0,0), (x, y+self.top_padding), (x+self.width, y+self.top_padding), self.thickness)
+        pygame.draw.line(screen, (0,0,0), (x, y+self.top_padding), (x+self.width-self.left_padding*2, y+self.top_padding), self.thickness)
