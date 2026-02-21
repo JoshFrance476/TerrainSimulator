d72b6bbd1f6b80d7ba6aa7e2e1847962419bf3ed
Josh France <joshfrance.476@gmail.com>
2026-02-21 12:00:33 +0000

    Modify prompt_new_interaction to handle exit flags from each option instead of a singular flag

--- storyteller/storyteller_manager.py
+++ storyteller/storyteller_manager.py
@@ -1,13 +1,13 @@
 from storyteller.scenario import Scenario
-from utils.llm_utils import prompt_scenario, prompt_notebook_setup, prompt_scenario_summary
+from utils.llm_utils import prompt_scenario, prompt_character_setup, prompt_scenario_summary
 
 class StorytellerManager:
     def __init__(self, controller):
         self.controller = controller
 
-        self.world_description = "A sparsely populated realm with strange, alien flora and fauna."
-        self.character_description = "A wandering traveler with expert knowledge of the alien world they live in"  #will just be used to generate an inital character notebook (and maybe history?)
-        self.story_focus_description = "The player will embark on an adventure across the realm, learning the secrets of the world. The player should not be given options to 'go along' with NPCs or work for them."
+        self.world_description = "An alient planet with strange, often dangerous flora and fauna."
+        self.character_description = "A space freighter crewmember who has crashlanded on an alien planet. Has no gear, and must fight for survival in an unfamiliar world"  #will just be used to generate an inital character notebook (and maybe history?)
+        self.story_focus_description = "The player has crashed landed on this alient planet. In their fight for survival they uncover the beautiful, intricate ecosystem that thrives on this planet."
 
         self.character_notebook = []
         self.character_history  = []
@@ -19,14 +19,14 @@ class StorytellerManager:
         self.input_token_count = 0
         self.output_token_count = 0
 
-        self.setup_notebook()
+        self.setup_notebook_and_stats()
     
     def update_token_counts(self, output_tokens, input_tokens):
         self.output_token_count += output_tokens
         self.input_token_count += input_tokens
 
-    def setup_notebook(self):
-        output_tokens, input_tokens, notebook = prompt_notebook_setup(self.character_description)
+    def setup_notebook_and_stats(self):
+        output_tokens, input_tokens, notebook, stat_list = prompt_character_setup(self.character_description, self.world_description, self.story_focus_description)
         self.update_token_counts(output_tokens, input_tokens)
 
         self.character_notebook = notebook
@@ -47,23 +47,23 @@ class StorytellerManager:
 
     def prompt_new_interaction(self):
         prompt = self.build_prompt(self.current_scenario)
-        output_tokens, input_tokens, description, exit_flag, actions = prompt_scenario(prompt, self.character_notebook)
+        output_tokens, input_tokens, description, actions = prompt_scenario(prompt, self.character_notebook)
         self.update_token_counts(output_tokens, input_tokens)
-        if exit_flag:
-            self.current_scenario.add_interaction(description, "End scenario")
+        if self.current_scenario:
+            self.current_scenario.set_pending_interaction(description, actions)
+        else:
+            new_scenario = self.create_scenario()
+            new_scenario.set_pending_interaction(description, actions)
+            self.current_scenario = new_scenario
+    
+    def submit_action(self, action_index):
+        self.current_scenario.submit_action(action_index)
+        if self.current_scenario.ended:
             output_tokens, input_tokens, self.character_notebook = prompt_scenario_summary(self.current_scenario.get_interactions_string(), self.character_notebook)
             self.update_token_counts(output_tokens, input_tokens)
         else:
-            if self.current_scenario:
-                self.current_scenario.set_pending_interaction(description, actions)
-            else:
-                new_scenario = self.create_scenario()
-                new_scenario.set_pending_interaction(description, actions)
-                self.current_scenario = new_scenario
-    
-    def submit_action(self, action_index):
-        self.current_scenario.sumbit_action(action_index)
-        self.prompt_new_interaction()
+            self.prompt_new_interaction()
+        
     
     def get_notebook(self):
         return self.character_notebook
