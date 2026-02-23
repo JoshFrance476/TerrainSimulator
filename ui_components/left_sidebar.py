cbe8ec7de424627cd37ccfb9302b60da490b914e
Josh France <joshfrance.476@gmail.com>
2026-02-23 19:58:00 +0000

    Change settings button to menu button

--- ui_components/left_sidebar.py
+++ ui_components/left_sidebar.py
@@ -18,7 +18,7 @@ class LeftSidebarController:
 
         self.navigation_bar = [[Button(65,20, lambda: self.show_biome_manager_page(), "Biome", self.fonts.small_font),
                                 Button(85,20, lambda: self.show_character_page(), "Character", self.fonts.small_font),
-                                Button(85,20, lambda: self.show_settings_page(), "Settings", self.fonts.small_font)],
+                                Button(85,20, lambda: self.controller.show_menu(), "Menu", self.fonts.small_font)],
                                 LineDivider(config.SIDEBAR_WIDTH, thickness=0, top_padding=10, bottom_padding=5)]
 
     def show_character_page(self):
@@ -132,25 +132,10 @@ class LeftSidebarController:
 
         title_label = Label("Settings", self.fonts.header, config.SIDEBAR_WIDTH)
 
-        save_map_label = Label("Save map", self.fonts.large_font, config.SIDEBAR_WIDTH)
-        save_file_name = TextBox(self.controller, self.fonts.small_font, 150, 20)
-        save_button = Button(60, 20, lambda: self.controller.save_map(save_file_name.text), "Save", self.fonts.small_font)
-
-        load_map_label = Label("Load map", self.fonts.large_font, config.SIDEBAR_WIDTH)
-        load_file_name = TextBox(self.controller, self.fonts.small_font, 150, 20)
-        load_button = Button(60,20, lambda: self.controller.load_map(load_file_name.text), "Load", self.fonts.small_font)
-
         self.component_list.extend(self.navigation_bar)
         self.component_list.append(title_label)
-        self.component_list.append(save_map_label)
-        self.component_list.append(save_file_name)
-        self.component_list.append(save_button)
 
-        self.component_list.append(LineDivider(config.SIDEBAR_WIDTH, top_padding=10, bottom_padding=10))
 
-        self.component_list.append(load_map_label)
-        self.component_list.append(load_file_name)
-        self.component_list.append(load_button)
 
 
     def clear_page(self):
