98145fc6725a780406419c930cd42b22cebd2c8d
Josh France <joshfrance.476@gmail.com>
2026-02-28 10:30:26 +0000

    Add AppState

---
+++ app_state.py
@@ -0,0 +1,18 @@
+class AppState:
+    def __init__(self):
+        self.selected_cell = None
+        self.hovered_cell = None
+        
+        self.selected_filter = "colour"
+
+        self.paused = True
+        self.interaction_type = "view_tile"
+
+        self.tile_paint_id = None
+        self.tile_paint_enabled = False
+
+        self.active_region_paint = None
+        self.most_recent_region_paint = None
+
+        self.focused_entity = None
+        self.ui_locked = False
