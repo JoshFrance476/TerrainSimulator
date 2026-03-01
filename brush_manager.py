e37dc3dbe45f4617fef7a8f967b9e744444a2382
Josh France <joshfrance.476@gmail.com>
2026-03-01 19:59:50 +0000

    Add 'negative' option to get_brush_mask

--- brush_manager.py
+++ brush_manager.py
@@ -3,29 +3,32 @@ import config
 
 class BrushManager:
     def __init__(self):
-        self.brush_size = 5
-        self.brush_strength = 0.02
+        self.size = 6
+        self.strength = 0.02
     
-    def get_brush(self, location):
-        brush_locations = []
-        for x in range(self.brush_size):
-            for y in range(self.brush_size):
-                brush_locations.append((location[0] + (1 - x), location[1] + (1 - y)))
-        return brush_locations
 
-    def get_brush_mask(self, location, max_strength, boolean=False):
+    def get_brush_mask(self, location, boolean=True, negative=False):
         y0, x0 = location
 
         yy, xx = np.ogrid[:config.WORLD_ROWS, :config.WORLD_COLS]
 
         dist = np.sqrt((xx - x0)**2 + (yy - y0)**2)
 
-        mask = 1 - (dist / self.brush_size)
+        mask = 1 - (dist / self.size) 
         mask = np.clip(mask, 0, 1)
 
         if boolean:
             return mask > 0
         else:
-            return mask * max_strength
+            if negative:
+                return mask * (-self.strength)
+            else:
+                return mask * self.strength
+    
+    def get_attributes(self):
+        return {
+            "size": self.size,
+            "strength": self.strength
+        }
     
     
