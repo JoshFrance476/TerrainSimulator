db29871f7d229217fadf14417058e047988aeee8
Josh France <joshfrance.476@gmail.com>
2026-03-01 19:58:25 +0000

    Modify region_manager to use bitmasks for adding and deleting regions

--- simulation/region_manager.py
+++ simulation/region_manager.py
@@ -1,9 +1,10 @@
 from simulation.region import Region
 from config import WORLD_COLS, WORLD_ROWS
+import numpy as np
 
 class RegionManager:
     def __init__(self):
-        self.region_map = [[[] for x in range(WORLD_COLS)] for y in range(WORLD_ROWS)] 
+        self.region_map = np.zeros((WORLD_ROWS, WORLD_COLS), dtype=np.uint64)
         self.region_list = []
         self.rid_counter = 0
 
@@ -14,26 +15,27 @@ class RegionManager:
         self.region_list.append(new_region)
         return new_region.rid
 
-    def add_region_to_location(self, location, rid):
-        grid_address = self.region_map[location[0]][location[1]]
-        if rid not in grid_address:
-            grid_address.append(rid)
+    def add_region_with_mask(self, mask, rid):
+        bit = np.uint64(1) << np.uint64(rid)
+        self.region_map[mask] |= bit
     
-    def remove_region_from_location(self, location, rid):
-        grid_address = self.region_map[location[0]][location[1]]
-        if rid in grid_address:
-            grid_address.remove(rid)
-        
-    
+    def remove_region_with_mask(self, mask, rid):
+        bit = np.uint64(1) << np.uint64(rid)
+        clear = np.uint64(~bit)
+        self.region_map[mask] &= clear
+
     def get_region_map(self):
         return self.region_map
     
     def get_regions_at_location(self, location):
+        mask = self.region_map[location]
         region_list = []
-        grid_address = self.region_map[location[0]][location[1]]
-        for rid in grid_address:
-            region = self.region_list[rid]
-            region_list.append(region)
+        bit = 0
+        while mask:
+            if mask & 1:
+                region_list.append(self.region_list[bit])
+            mask >>= 1
+            bit += 1
         return region_list
 
     def get_region(self, rid):
