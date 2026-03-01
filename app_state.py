2837feaf98928d27409d037e0bf3173a2c85c1d7
Josh France <joshfrance.476@gmail.com>
2026-03-01 12:27:03 +0000

    Change pages and interaction types from strings to enums

--- app_state.py
+++ app_state.py
@@ -1,9 +1,11 @@
+from enum import Enum, auto
+
 class AppState:
     def __init__(self):
         self.selected_cell = None
         self.hovered_cell = None
 
-        self.interaction_type = "view_tile"
+        self.interaction_type = InteractionType.VIEW_TILE
 
         self.tile_paint_id = None
         self.tile_paint_enabled = False
@@ -14,8 +16,8 @@ class AppState:
         self.focused_entity = None
         self.ui_locked = False
 
-        self.left_page = "biome_editor"
-        self.right_page = "scenario"
+        self.left_page = LeftPage.BIOME_EDITOR
+        self.update_right_page = False
 
         self.show_menu = False
 
@@ -24,3 +26,21 @@ class AppState:
 
         self.left_mouse_down = False
         self.right_mouse_down = False
+
+
+class InteractionType(Enum):
+    VIEW_TILE = auto()
+    MOVE_PLAYER = auto()
+    PAINT_REGION = auto()
+    PAINT_TILE = auto()
+    EDIT_ELEVATION = auto()
+
+class LeftPage(Enum):
+    VIEW_LOCATION = auto()
+    BIOME_EDITOR = auto()
+    TILE_EDITOR = auto()
+    REGION_EDITOR = auto()
+    VIEW_CHARACTER = auto()
+
+class RightPage(Enum):
+    VIEW_SCENARIO = auto()
