1563172d280466b7e63607ebdee6d0e5cb2d5c7f
Josh France <joshfrance.476@gmail.com>
2026-03-03 17:01:19 +0000

    Separate brush window into independent pages for each type of brush interaction (elevation, region, biome)

--- ui_components/brush_window.py
+++ ui_components/brush_window.py
@@ -2,6 +2,8 @@ import pygame
 import config
 from ui_components.widgets.slider import Slider
 from ui_components.widgets.button import Button
+from ui_components.widgets.label import Label
+from app_state import InteractionType
 
 class BrushWindow:
     def __init__(self, fonts, interaction_system):
@@ -9,15 +11,46 @@ class BrushWindow:
         self.fonts = fonts
         self.interaction_system = interaction_system
 
-        self.rect = pygame.Rect(config.SIDEBAR_WIDTH, 0, 200, 60)
-
+        self.rect = pygame.Rect(config.SIDEBAR_WIDTH, 0, 200, 100)
+    
+    def show_page(self, page_name):
+        match page_name:
+            case InteractionType.PAINT_REGION:
+                self.show_region_brush()
+            case InteractionType.PAINT_TILE:
+                self.show_biome_brush()
+            case InteractionType.EDIT_ELEVATION:
+                self.show_elevation_brush()
+    
+    def show_biome_brush(self):
+        self.component_list = []
+        title = Label("Biome Brush", self.fonts.small_font)
         self.brush_size_slider = Slider(self.fonts.small_font, 1, 10, 100, top_padding=10)
-        self.submit_button = Button(50, 20, lambda: self.interaction_system.set_brush_attributes(size=self.brush_size_slider.value), "Set", self.fonts.small_font)
+        submit_button = Button(50, 20, lambda: self.interaction_system.set_brush_attributes(size=self.brush_size_slider.value), "Set", self.fonts.small_font)
+        self.component_list.append(title)
         self.component_list.append(self.brush_size_slider)
-        self.component_list.append(self.submit_button)
+        self.component_list.append(submit_button)
+    
+    def show_elevation_brush(self):
+        self.component_list = []
+        title = Label("Elevation Brush", self.fonts.small_font)
+        self.brush_size_slider = Slider(self.fonts.small_font, 1, 10, 100, top_padding=10)
+        submit_button = Button(50, 20, lambda: self.interaction_system.set_brush_attributes(size=self.brush_size_slider.value), "Set", self.fonts.small_font)
+        self.component_list.append(title)
+        self.component_list.append(self.brush_size_slider)
+        self.component_list.append(submit_button)
+    
+    def show_region_brush(self):
+        self.component_list = []
+        title = Label("Region Brush", self.fonts.small_font)
+        self.brush_size_slider = Slider(self.fonts.small_font, 1, 10, 100, top_padding=10)
+        submit_button = Button(50, 20, lambda: self.interaction_system.set_brush_attributes(size=self.brush_size_slider.value), "Set", self.fonts.small_font)
+        self.component_list.append(title)
+        self.component_list.append(self.brush_size_slider)
+        self.component_list.append(submit_button)
 
     def set_attributes(self, attributes_dict):
-        self.brush_size_slider.default_value = attributes_dict['size']
+        self.brush_size_slider.value = attributes_dict['size']
 
     def draw(self, screen):
         pygame.draw.rect(screen, (220,220,220), self.rect)
