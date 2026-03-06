916b0fe15de17d7fba670865099311c8b613f3f5
Josh France <joshfrance.476@gmail.com>
2026-03-06 14:14:19 +0000

    Modify chunk map generation to set ocean to 0 instead of -1

--- simulation/chunk_manager.py
+++ simulation/chunk_manager.py
@@ -5,11 +5,14 @@ from utils.colour_utils import hsv_to_rgb_array
 
 class ChunkManager:
     def __init__(self, rows, cols):
-        self.chunk_map = np.ndarray((rows, cols), dtype=np.int16)
+        self.chunk_map = np.full((rows, cols), -1, dtype=np.int16)
         self.chunks = {}
 
     
     def generate_chunk_map(self, colour_map, biome_map):
+        self.chunks = {}
+        
+
         ocean_mask = (biome_map == 0)
 
         colour_map[ocean_mask] = (0,0,0)
@@ -17,8 +20,8 @@ class ChunkManager:
 
         # Chunking algorithm
         chunked_map = quickshift(rgb_colour_map, kernel_size=1, max_dist=5, ratio=0.2)
-
-        chunked_map[ocean_mask] = -1
+        chunked_map = chunked_map.astype(np.int16) + 1
+        chunked_map[ocean_mask] = 0
 
         chunk_props = regionprops(chunked_map)
 
