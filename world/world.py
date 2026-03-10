20f14a085359f1d58dd0e7e83c360019512c83bd
Josh France <joshfrance.476@gmail.com>
2026-03-10 17:48:09 +0000

    Organise world into clear function categories

--- world/world.py
+++ world/world.py
@@ -8,147 +8,23 @@ import json
 from pathlib import Path
 
 class World:
+    """
+    Organisation:
+    Loading/Saving
+    World Data
+    Biome Config
+    Chunks
+    Regions
+    """
     def __init__(self, rows, cols):
         self.rows, self.cols = rows, cols
-        self.tick_count = 0
+        self.data = None
+        self.region_manager = None
+        self.chunk_manager = None
+        self.biome_config = None
 
-    def get_region_map(self): 
-        return self.region_manager.get_region_map()
 
-    def get_chunk_map(self):
-        return self.chunk_manager.get_chunk_map()
-
-    def get_chunk_id_at(self, location):
-        return self.chunk_manager.get_id_at(location)
-    
-    def get_closest_chunks(self, location, count=5):
-        return self.chunk_manager.get_closest_chunks(location, count)
-
-    def get_semantic_chunk_context(self, location):
-        return self.chunk_manager.get_semantic_surroundings(location)
-    
-    def get_biome_data_at_location(self, location):
-        return self.biome_config.biomes[(self.get_cell_data(location)['biome'])]
-    
-    def get_biome_data_from_id(self, biome_id):
-        return self.biome_config.biomes[biome_id]
-
-    def get_biomes(self):
-        return self.biome_config.biomes
-
-    def get_starting_location(self):
-        return self.biome_config.get_starting_location()
-        
-    def step(self):
-        self.tick_count += 1
-    
-    def get_semantic_tile_data(self, location):
-        info = {
-            "Biome": self.get_biome_at(location),
-            "Details": {}
-        }
-        for region in self.region_manager.get_regions_at_location(location):
-            info["Details"][region.title] = {
-                "Visible Description": region.visible_desc,
-                "Hidden Description": region.hidden_desc
-            }
-        return info
-
-    def get_regions_at_location(self, location):
-        return self.region_manager.get_regions_at_location(location)
-
-    def get_region(self, region_id):
-        return self.region_manager.get_region(region_id)
-    
-    def apply_edit_elevation_mask(self, mask):
-        self.data.world_data['elevation'] += mask
-    
-    def apply_smoothing_elevation_mask(self, mask):
-        elevation = self.data.world_data['elevation']
-        selected = elevation[mask]
-
-        average = selected.mean()
-
-        elevation[mask] = elevation[mask] + 0.1 * (average - elevation[mask])
-
-    def update_steepness(self):
-        self.data.update_steepness()
-    
-    def update_biome(self, mask=None):
-        self.data.update_biome(mask)
-    
-    def update_stage_3(self):
-        self.data.update_stage_3()
-    
-    def set_biome_with_mask(self, mask, biome_id):
-        self.data.set_biome_with_mask(mask, biome_id)
-
-    def add_region_with_mask(self, mask, region_id):
-        self.region_manager.add_region_with_mask(mask, region_id)
-
-    def remove_region_with_mask(self, mask, region_id):
-        self.region_manager.remove_region_with_mask(mask, region_id)
-    
-    def create_region(self):
-        return self.region_manager.create_region()
-
-    def add_new_region_to_chunk(self, chunk_id, title, visible_desc, hidden_desc):
-        location = self.chunk_manager.get_random_location_in_chunk(chunk_id)
-        self.region_manager.create_region_at_location(location, title, visible_desc, hidden_desc)
-
-    def get_world_data(self):
-        return self.data.get_world_data()  
-    
-    def get_map_data(self, map_name):
-        return self.data.get_world_data()[map_name]
-
-    def add_biome(self, name, h, s, v, trav_cost):
-        self.biome_config.add_biome(name, h, s, v, trav_cost)
-    
-    def edit_biome(self, biome_index, new_name, new_h, new_s, new_v, new_trav_cost):
-        self.biome_config.edit_biome(biome_index, new_name, new_h, new_s, new_v, new_trav_cost)
-    
-    def set_map_data(self, map_name, data):
-        self.data.set_map_data(map_name, data)
-    
-    def set_map_data_at(self, map_name, pos, data):
-        self.data.set_map_data_at(map_name, pos, data)
-    
-    def get_biome_data(self, x0, y0, x1, y1):
-        return self.data.get_biome_data(x0, y0, x1, y1)
-
-    def get_biome_at(self, location):
-        return self.data.get_biome_at(location)
-
-    def get_cell_data(self, selected_cell):
-        if selected_cell:
-            return self.data.get_cell_data(selected_cell)
-        else:
-            return None
-    
-    def get_biome_name_from_id(self, biome_id):
-        return self.data.get_biome_name_from_id(biome_id)
-
-    def save_map(self, file_name, starting_location):
-        path = Path("data/saved_maps") / file_name
-        path.mkdir(parents=True, exist_ok=True)
-
-        self.biome_config.set_starting_location(starting_location)
-        np.savez(
-            path / file_name,
-            # numeric maps
-            **self.data.world_data,
-
-            # region manager (python objects)
-            region_map=np.array(self.region_manager.region_map, dtype=object),
-            region_list=np.array(self.region_manager.region_list, dtype=object),
-        )
-        biome_config = {
-                "constants": self.biome_config.constants,
-                "biomes": self.biome_config.biomes}
-        
-        with open(f'{path/file_name}.json', 'w') as f:
-            json.dump(biome_config, f)
+    # Loading/Saving ############################################################################################
 
 
     def load_map(self, file_name):
@@ -181,13 +57,152 @@ class World:
         self.region_manager = RegionManager(self.rows, self.cols, region_map, region_list, rid_counter)
 
         self.chunk_manager = ChunkManager(self.rows, self.cols, self.get_map_data("colour").copy(), self.get_map_data("biome").copy())
-
-
-
-
-
-
-
     
 
+    def save_map(self, file_name, starting_location):
+        path = Path("data/saved_maps") / file_name
+        path.mkdir(parents=True, exist_ok=True)
 
+        self.biome_config.set_starting_location(starting_location)
+        np.savez(
+            path / file_name,
+            # numeric maps
+            **self.data.world_data,
+
+            # region manager (python objects)
+            region_map=np.array(self.region_manager.region_map, dtype=object),
+            region_list=np.array(self.region_manager.region_list, dtype=object),
+        )
+        biome_config = {
+                "constants": self.biome_config.constants,
+                "biomes": self.biome_config.biomes}
+        
+        with open(f'{path/file_name}.json', 'w') as f:
+            json.dump(biome_config, f)
+
+
+    # World Data ############################################################################################
+
+
+    def apply_edit_elevation_mask(self, mask):
+        self.data.world_data['elevation'] += mask
+    
+    def apply_smoothing_elevation_mask(self, mask):
+        elevation = self.data.world_data['elevation']
+        selected = elevation[mask]
+
+        average = selected.mean()
+
+        elevation[mask] = elevation[mask] + 0.1 * (average - elevation[mask])
+
+    def update_steepness(self):
+        self.data.update_steepness()
+    
+    def update_biome(self, mask=None):
+        self.data.update_biome(mask)
+    
+    def update_stage_3(self):
+        self.data.update_stage_3()
+    
+    def set_biome_with_mask(self, mask, biome_id):
+        self.data.set_biome_with_mask(mask, biome_id)
+    
+    def get_world_data(self):
+        return self.data.get_world_data()  
+    
+    def get_map_data(self, map_name):
+        return self.data.get_world_data()[map_name]
+    
+    def set_map_data(self, map_name, data):
+        self.data.set_map_data(map_name, data)
+    
+    def set_map_data_at(self, map_name, pos, data):
+        self.data.set_map_data_at(map_name, pos, data)
+    
+    def get_biome_data(self, x0, y0, x1, y1):
+        return self.data.get_biome_data(x0, y0, x1, y1)
+
+    def get_biome_at(self, location):
+        return self.data.get_biome_at(location)
+
+    def get_cell_data(self, selected_cell):
+        if selected_cell:
+            return self.data.get_cell_data(selected_cell)
+        else:
+            return None
+    
+    def get_semantic_tile_data(self, location):
+        info = {
+            "Biome": self.get_biome_at(location),
+            "Details": {}
+        }
+        for region in self.region_manager.get_regions_at_location(location):
+            info["Details"][region.title] = {
+                "Visible Description": region.visible_desc,
+                "Hidden Description": region.hidden_desc
+            }
+        return info
+
+
+    # Biome Config ############################################################################################
+
+
+    def get_biome_data_at_location(self, location):
+        return self.biome_config.biomes[(self.get_cell_data(location)['biome'])]
+    
+    def get_biome_data_from_id(self, biome_id):
+        return self.biome_config.biomes[biome_id]
+
+    def get_biomes(self):
+        return self.biome_config.biomes
+
+    def get_starting_location(self):
+        return self.biome_config.get_starting_location()
+
+    def add_biome(self, name, h, s, v, trav_cost):
+        self.biome_config.add_biome(name, h, s, v, trav_cost)
+    
+    def edit_biome(self, biome_index, new_name, new_h, new_s, new_v, new_trav_cost):
+        self.biome_config.edit_biome(biome_index, new_name, new_h, new_s, new_v, new_trav_cost)
+    
+
+    # Chunks ############################################################################################
+
+
+    def get_chunk_map(self):
+        return self.chunk_manager.get_chunk_map()
+
+    def get_chunk_id_at(self, location):
+        return self.chunk_manager.get_id_at(location)
+    
+    def get_closest_chunks(self, location, count=5):
+        return self.chunk_manager.get_closest_chunks(location, count)
+
+    def get_semantic_chunk_context(self, location):
+        return self.chunk_manager.get_semantic_surroundings(location)
+
+
+    # Regions ##############################################################################################
+
+
+    def get_region_map(self): 
+        return self.region_manager.get_region_map()
+    
+    def get_regions_at_location(self, location):
+        return self.region_manager.get_regions_at_location(location)
+
+    def get_region(self, region_id):
+        return self.region_manager.get_region(region_id)
+    
+    def add_region_with_mask(self, mask, region_id):
+        self.region_manager.add_region_with_mask(mask, region_id)
+
+    def remove_region_with_mask(self, mask, region_id):
+        self.region_manager.remove_region_with_mask(mask, region_id)
+    
+    def create_region(self):
+        return self.region_manager.create_region()
+
+    def add_new_region_to_chunk(self, chunk_id, title, visible_desc, hidden_desc):
+        location = self.chunk_manager.get_random_location_in_chunk(chunk_id)
+        self.region_manager.create_region_at_location(location, title, visible_desc, hidden_desc)
