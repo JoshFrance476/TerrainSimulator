8962f6c41072f222d3cd588a63526ca2f25d7efb
Josh France <joshfrance.476@gmail.com>
2026-03-18 14:02:42 +0000

    Add functions to get and set story descriptions

--- storytelling/story_state.py
+++ storytelling/story_state.py
@@ -1,10 +1,8 @@
 class StoryState:
     def __init__(self):
-        self.world_description = """The world is abandoned and desolate. The remaining structures are in ruins. There are no signs remaining humans or animals.
-        There is a constant sense of danger and tension even though no life forms are encountered. 20th Soviet-style architecture.
-        """
-        self.character_description = """A lone wanderer, not sure what to make of the world around him. The character is paranoid and has a sense that there is someone or something lurking nearby, influencing the world but just out of sight"""  
-        self.story_focus_description = """The environment is vast and empty. There is a constant sense of tension as the player travels across the land."""
+        self.world_description = ""
+        self.character_description = ""  
+        self.story_focus_description = ""
 
         self.notebook = []
         self.character_history = []
@@ -17,3 +15,15 @@ class StoryState:
 
         self.completion_tokens = 0
         self.prompt_tokens = 0
+    
+    def get_setup(self):
+        return {
+            "world_description": self.world_description, 
+            "character_description": self.character_description, 
+            "story_focus_description": self.story_focus_description
+        }
+    
+    def setup(self, story_setup):
+        self.world_description = story_setup['world_description']
+        self.character_description = story_setup['character_description']
+        self.story_focus_description = story_setup['story_focus_description']
