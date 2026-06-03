192df078a509184afee59eb6c7b514e8dee1f425
Josh France <joshfrance.476@gmail.com>
2026-06-03 11:24:39 +0100

    Rename scenario.py to scene.py

--- storytelling/scene.py
+++ storytelling/scene.py
@@ -8,8 +8,8 @@ class Scenario:
         self.guide = guide
         self.interaction_count = 0
     
-    def add_interaction(self, description, decision):
-        interaction = CompletedInteraction(description, decision)
+    def add_interaction(self, description, action, outcome):
+        interaction = CompletedInteraction(description, action, outcome)
         self.completed_interactions.append(interaction)
         self.interaction_count += 1
         self.pending_interaction = None
@@ -24,15 +24,15 @@ class Scenario:
                                  "Action": interaction.decision})
         return interactions
 
-    def set_pending_interaction(self, description, actions):
-        self.pending_interaction = PendingInteraction(description, actions)
+    def set_pending_interaction(self, description, actions, outcomes):
+        self.pending_interaction = PendingInteraction(description, actions, outcomes)
     
-    def submit_action(self, action_index):
+    def submit_action(self, action):
         description = self.pending_interaction.description
-        action = self.pending_interaction.actions[action_index]
-        self.add_interaction(description, action['action'])
-        if action['exit_flag']:
+        outcome = self.pending_interaction.action_table[action]["outcome"]
+        if self.pending_interaction.action_table[action]["exit_flag"]:
             self.end()
+        self.add_interaction(description, action, outcome)
     
     def submit_custom_action(self, action_desc):
         description = self.pending_interaction.description
@@ -43,3 +43,15 @@ class Scenario:
             return self.completed_interactions[len(self.completed_interactions)-1].decision
         else:
             return None
+    
+    def get_most_recent_outcome(self):
+        if len(self.completed_interactions) > 0:
+            return self.completed_interactions[len(self.completed_interactions)-1].outcome
+        else:
+            return None
+
+    def get_outcomes(self):
+        outcome_list = []
+        for interaction in self.completed_interactions:
+            outcome_list.append(interaction.outcome)
+        return outcome_list[::-1]
