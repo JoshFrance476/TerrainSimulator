e9097823ecb019d577c7d3e7e5adb481da1a0904
Josh France <joshfrance.476@gmail.com>
2026-06-03 11:25:24 +0100

    Replace actions with action_table in pending and add outcome to completed

--- storytelling/interaction.py
+++ storytelling/interaction.py
@@ -1,10 +1,22 @@
 class CompletedInteraction:
-    def __init__(self, description, decision):
+    def __init__(self, description, decision, outcome):
         self.description = description
         self.decision = decision
+        self.outcome = outcome
 
 class PendingInteraction:
-    def __init__(self, description, actions):
+    def __init__(self, description, actions, outcomes):
         self.description = description
-        self.actions = actions
+        self.action_table = {}
+
+        if len(actions) != len(outcomes):
+            print(actions, outcomes)
+            raise ValueError("Error: Action count doesn't match outcome count.")
+        
+        
+        for action, outcome in zip(actions, outcomes):
+            self.action_table[action['action']] = {
+                "exit_flag": action['exit_flag'],
+                "outcome": outcome
+                }
     
