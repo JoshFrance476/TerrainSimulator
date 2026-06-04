48ea4e299d97b495b5367c229d8077d2612bff33
Josh France <joshfrance.476@gmail.com>
2026-06-04 13:13:53 +0100

    Simplify Scene class

--- storytelling/scene.py
+++ storytelling/scene.py
@@ -1,57 +1,46 @@
-from storytelling.interaction import CompletedInteraction, PendingInteraction
+from storytelling.interaction import Interaction
 
-class Scenario:
-    def __init__(self, guide=""):
+class Scene:
+    def __init__(self):
         self.completed_interactions = []
         self.pending_interaction = None
         self.ended = False
-        self.guide = guide
         self.interaction_count = 0
     
-    def add_interaction(self, description, action, outcome):
-        interaction = CompletedInteraction(description, action, outcome)
-        self.completed_interactions.append(interaction)
-        self.interaction_count += 1
-        self.pending_interaction = None
-    
-    def end(self):
+    def end_scene(self):
         self.ended = True
     
     def get_interactions_json(self):
         interactions = []
         for interaction in self.completed_interactions:
             interactions.append({"Situation": interaction.description,
-                                 "Action": interaction.decision})
+                                 "Action": interaction.chosen_action})
         return interactions
 
-    def set_pending_interaction(self, description, actions, outcomes):
-        self.pending_interaction = PendingInteraction(description, actions, outcomes)
+    def set_pending_interaction(self, description, actions, outcomes, guide):
+        self.pending_interaction = Interaction(description, actions, outcomes, guide)
     
     def submit_action(self, action):
-        description = self.pending_interaction.description
-        outcome = self.pending_interaction.action_table[action]["outcome"]
-        if self.pending_interaction.action_table[action]["exit_flag"]:
-            self.end()
-        self.add_interaction(description, action, outcome)
-    
-    def submit_custom_action(self, action_desc):
-        description = self.pending_interaction.description
-        self.add_interaction(description, action_desc)
-    
-    def get_most_recent_action(self):
-        if len(self.completed_interactions) > 0:
-            return self.completed_interactions[len(self.completed_interactions)-1].decision
-        else:
-            return None
-    
-    def get_most_recent_outcome(self):
-        if len(self.completed_interactions) > 0:
-            return self.completed_interactions[len(self.completed_interactions)-1].outcome
-        else:
-            return None
+        self.pending_interaction.set_chosen_action(action)
+        if self.pending_interaction.ends_scene():
+            self.end_scene()
+        self.completed_interactions.append(self.pending_interaction)
+        self.interaction_count += 1
+        self.pending_interaction = None
 
     def get_outcomes(self):
         outcome_list = []
         for interaction in self.completed_interactions:
             outcome_list.append(interaction.outcome)
         return outcome_list[::-1]
+    
+    def get_scene_history(self):
+        return {
+            f"scene{idx}": {
+                "environment_description": interaction.guide["environment_description"],
+                "precise_location": interaction.guide["precise_location"],
+                "story": interaction.guide["story_suggestion"],
+                "action": interaction.outcome
+            }
+            for idx, interaction in enumerate(reversed(self.completed_interactions))
+        }
