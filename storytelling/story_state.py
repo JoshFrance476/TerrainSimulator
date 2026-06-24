6c99690843bb72a00e65785f0953b0647d6a7c8a
josh france <joshfrance.476@gmail.com>
2026-06-24 11:08:26 +0100

    Add required attributes for handling async streamed LLM responses

--- storytelling/story_state.py
+++ storytelling/story_state.py
@@ -1,3 +1,5 @@
+import queue
+
 class StoryState:
     def __init__(self):
         self.world_description = ""
@@ -12,6 +14,10 @@ class StoryState:
         self.movement_history = [] # Dicts containing "direction" and "biome"
 
         self.current_scene = None
+
+        self.chunk_queue = queue.Queue()
+        self.is_streaming = False
+        self.stream_response = ""
 
         self.completion_tokens = 0
         self.prompt_tokens = 0
