1ca883560c6bfdc8e04de0bd261ea3b07a2f5b5d
Josh France <joshfrance.476@gmail.com>
2026-07-18 15:38:11 +0100

    Add mapDisplay

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from 'react'
 import ChatWindow from './components/ChatWindow'
+import MapDisplay from './components/MapDisplay'
 
 function App() {
-  return <ChatWindow />
+  return (
+    <div className="display">
+      <ChatWindow />
+      <MapDisplay />
+    </div>
+  )
 }
 
 export default App
