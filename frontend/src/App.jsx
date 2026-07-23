a59fc45296fd70ca96e588f314b600a4887d72ea
Josh France <joshfrance.476@gmail.com>
2026-07-23 15:16:34 +0100

    Add InfoWindow and a selectedCell const

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import ChatWindow from './components/ChatWindow'
 import MapDisplay from './components/MapDisplay'
+import InfoWindow from './components/InfoWindow'
 
 function App() {
+  const [selectedCell, setSelectedCell] = useState(null) // shape: {x, y, biomeData}
+
   return (
     <div className="display">
       <ChatWindow />
-      <MapDisplay />
+      <InfoWindow 
+        selectedCell={selectedCell} 
+      />
+      <MapDisplay 
+        selectedCell={selectedCell}
+        onCellSelect={setSelectedCell}
+      />
     </div>
   )
 }
