a8aaafe850f82edf6e5125d935e6ed338d71a22f
Josh France <joshfrance.476@gmail.com>
2026-07-24 14:55:41 +0100

    Add regions to info window

--- frontend/src/components/InfoWindow.jsx
+++ frontend/src/components/InfoWindow.jsx
@@ -3,8 +3,15 @@ function InfoWindow({ selectedCell }) {
         <div className="info-window">
             {selectedCell && (
                 <div>
-                    <h3 className="capitalise">{selectedCell.biomeData.name}</h3>
-                    <p>Position: ({selectedCell.x}, {selectedCell.y})</p>
+                    <p className="region-position">Position: ({selectedCell.x}, {selectedCell.y})</p>
+                    <h2 className="capitalise biome-name">{selectedCell.biomeData.name}</h2>
+                    {selectedCell.regionData.map((region, index) => (
+                        <div key={index} className="region-info"> 
+                            <h3 className="capitalise region-title">{region.title}</h3>
+                            <p className="region-visible-desc">{region.visible_desc}</p>
+                            <p className="region-hidden-desc"><i>{region.hidden_desc}</i></p>
+                        </div>
+                    ))}
                 </div>
             )}
         </div>
