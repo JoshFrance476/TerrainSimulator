31f56efe9233817d679c6847aaeda2c96909b02e
Josh France <joshfrance.476@gmail.com>
2026-07-28 22:38:32 +0100

    Simplified interactions into single array

--- backend/storytelling/scene.py
+++ backend/storytelling/scene.py
@@ -2,10 +2,17 @@ from storytelling.interaction import Interaction
 
 class Scene:
     def __init__(self):
-        self.completed_interactions = []
-        self.pending_interaction = None
+        self.interactions = []
         self.ended = False
-        self.interaction_count = 0
+
+    @property
+    def pending_interaction(self):
+        last = self.interactions[-1] if self.interactions else None
+        return last if last and not last.is_complete else None
+
+    @property
+    def completed_interactions(self):
+        return [i for i in self.interactions if i.is_complete]
     
     def end_scene(self):
         self.ended = True
@@ -17,31 +24,29 @@ class Scene:
                                  "Action": interaction.chosen_action})
         return interactions
 
-    def set_pending_interaction(self, description, actions, outcomes, guide):
-        self.pending_interaction = Interaction(description, actions, outcomes, guide)
+    def add_interaction(self, description, actions, outcomes, guide):
+        self.interactions.append(Interaction(description, actions, outcomes, guide))
     
     def submit_action(self, action):
-        self.pending_interaction.set_chosen_action(action)
-        if self.pending_interaction.ends_scene():
+        interaction = self.pending_interaction
+        interaction.set_chosen_action(action)
+        if interaction.ends_scene():
             self.end_scene()
-        self.completed_interactions.append(self.pending_interaction)
-        self.interaction_count += 1
-        self.pending_interaction = None
 
-    def get_outcomes(self):
-        outcome_list = []
-        for interaction in self.completed_interactions:
-            outcome_list.append(interaction.outcome)
-        return outcome_list[::-1]
-    
-    def get_scene_history(self):
+    def to_dict(self):
         return {
-            f"scene_{idx}": {
-                "environment_description": interaction.guide["environment_description"],
-                "precise_location": interaction.guide["precise_location"],
-                "story": interaction.guide["story_suggestion"],
-                "action": interaction.outcome
-            }
-            for idx, interaction in enumerate(self.completed_interactions)
+            "interactions": [i.to_dict() for i in self.interactions],
+            "ended": self.ended,
         }
+
+    def get_history(self):
+        history = []
+        for interaction in self.interactions:
+            history.append({ 
+                "description": interaction.description,
+                "chosen_action": interaction.chosen_action,
+                "outcome": interaction.outcome
+            })
+        return history
+                
     
