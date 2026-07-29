efd64b883711729b6d11ee5cc99a119577b09d42
Josh France <joshfrance.476@gmail.com>
2026-07-29 11:52:34 +0100

    Add world data context and story data context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,26 +2,32 @@ import { useState } from 'react'
 import StoryWindow from './components/StoryWindow'
 import MapDisplay from './components/MapDisplay'
 import InfoWindow from './components/InfoWindow'
+import { WorldDataProvider } from './contexts/WorldDataContext'
+import { StoryDataProvider } from './contexts/StoryDataContext'
 
 function App() {
   const [selectedCell, setSelectedCell] = useState(null) // shape: {x, y, biomeData, regionData}
   const [playerLocation, setPlayerLocation] = useState(null) // shape: {x, y}
 
   return (
-    <div className="display">
-      <StoryWindow 
-        playerLocation={playerLocation}
-      />
-      <InfoWindow 
-        selectedCell={selectedCell} 
-      />
-      <MapDisplay 
-        selectedCell={selectedCell}
-        onCellSelect={setSelectedCell}
-        playerLocation={playerLocation}
-        onPlayerLocationChange={setPlayerLocation}
-      />
-    </div>
+    <WorldDataProvider>
+      <StoryDataProvider>
+        <div className="display">
+          <StoryWindow 
+            playerLocation={playerLocation}
+          />
+          <InfoWindow 
+            selectedCell={selectedCell} 
+          />
+          <MapDisplay 
+            selectedCell={selectedCell}
+            onCellSelect={setSelectedCell}
+            playerLocation={playerLocation}
+            onPlayerLocationChange={setPlayerLocation}
+          />
+        </div>
+      </StoryDataProvider>
+    </WorldDataProvider>
   )
 }
 
