a9f15222bb1556fd3c76fb44296910af461af148
Josh France <joshfrance.476@gmail.com>
2026-07-29 11:55:01 +0100

    Replace test data with a quest list from storyDataContext

--- frontend/src/components/info-tabs/QuestTab.jsx
+++ frontend/src/components/info-tabs/QuestTab.jsx
@@ -1,40 +1,21 @@
 import { useState } from "react";
+import { useStoryDataContext } from "../../contexts/StoryDataContext";
 
 function QuestTab() {
-    const [quests, setQuests] = useState([
-        {
-            title: "The Lost Artifact",
-            description: "You learned of a mysterious artifact hidden in the nearby cave. Find it and bring it back to the village elder.",
-            objectives: [
-                "Find the cave entrance.",
-                "Find out more information on the artifact. (optional)",
-                "Get better equipped. (optional)"
-            ]
-        },
-        {
-            title: "Rescue the Villagers",
-            description: "Save the villagers captured by the bandits in the Dark Forest.",
-            objectives: [
-                "Find the bandits' hideout.",
-            ]   
-        }
-    ]); 
+    const { questsList } = useStoryDataContext()
+    if (!questsList) {
+        return <p>No quests</p>;
+    }
     return (
         <div>
-            {quests.length === 0 ? (
+            {questsList && questsList.length === 0 ? (
                 <p>No quests available.</p> 
             ) : (    
-                quests.map((quest, index) => (
+                questsList.map((quest, index) => (
                     <div key={index} className="info-window-box">
                         <h3 className="capitalise box-title">{quest.title}</h3>
-                        <p className="quest-description">{quest.description}</p>
-                        <div className="quest-objectives">
-                            <ul>
-                                {quest.objectives && quest.objectives.map((objective, objIndex) => (
-                                    <li key={objIndex}><i>{objective}</i></li>
-                                ))}
-                            </ul>
-                        </div>
+                        <p className="quest-visible-desc">{quest.visible_context}</p>
+                        <div className="quest-hidden-desc">{quest.hidden_context}</div>
                     </div>
                 ))
             )}
