e75e0faf1e09dc6c63250be0a61d388a94996ed9
Josh France <joshfrance.476@gmail.com>
2026-08-01 16:12:33 +0100

    Refactor App component: remove unused playerLocation state and context providers; simplify component structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,32 +2,25 @@ import { useState } from 'react'
 import StoryWindow from './components/StoryWindow'
 import MapDisplay from './components/MapDisplay'
 import InfoWindow from './components/InfoWindow'
-import { WorldDataProvider } from './contexts/WorldDataContext'
-import { StoryDataProvider } from './contexts/StoryDataContext'
+import Header from './components/Header'
 
 function App() {
   const [selectedCell, setSelectedCell] = useState(null) // shape: {x, y, biomeData, regionData}
-  const [playerLocation, setPlayerLocation] = useState(null) // shape: {x, y}
 
   return (
-    <WorldDataProvider>
-      <StoryDataProvider>
-        <div className="display">
-          <StoryWindow 
-            playerLocation={playerLocation}
-          />
-          <InfoWindow 
-            selectedCell={selectedCell} 
-          />
-          <MapDisplay 
-            selectedCell={selectedCell}
-            onCellSelect={setSelectedCell}
-            playerLocation={playerLocation}
-            onPlayerLocationChange={setPlayerLocation}
-          />
-        </div>
-      </StoryDataProvider>
-    </WorldDataProvider>
+    <>
+      <Header />
+      <div className="display">
+        <StoryWindow />
+        <InfoWindow 
+          selectedCell={selectedCell} 
+        />
+        <MapDisplay 
+          selectedCell={selectedCell}
+          onCellSelect={setSelectedCell}
+        />
+      </div>
+    </>
   )
 }
 
