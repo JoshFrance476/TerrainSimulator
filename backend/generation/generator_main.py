232126d9e6bc214ab7afcf6057a3a5ea0b309bf4
Josh France <joshfrance.476@gmail.com>
2026-08-08 14:05:03 +0100

    Remove conditional logging setup from generator_main.py

--- backend/generation/generator_main.py
+++ backend/generation/generator_main.py
@@ -11,9 +11,6 @@ from generation.stage_1_generator import calculate_temperature
 from generation.stage_2_generator import calculate_steepness, generate_sea_map, generate_rivers_map
 from generation.stage_3_generator import calculate_proximity_map, determine_biome, calculate_traversal_cost
 
-if config.LOGGING:
-    logging.basicConfig(level=logging.DEBUG)
-
 
 def generate_data_maps(rows, cols, biome_config):
 
