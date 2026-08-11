3cc9c3ed663e644690e97618373556723c15f192
Josh France <joshfrance.476@gmail.com>
2026-08-11 18:04:20 +0100

    Add handlePlay function to manage new session and update page state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import { useState, useEffect } from 'react'
 import Landing from './pages/Landing'
 import Play from './pages/Play'
+import { useNewSessionMutation } from './queries/queries'
 
 function App() {
   const [user, setUser] = useState(undefined); // undefined = loading, null = logged out
   const [page, setPage] = useState("landing");
+  const newSession = useNewSessionMutation()
+  
+  function handlePlay(worldId) {
+    newSession.mutate(worldId, {
+      onSuccess: () => {
+        setPage('play')
+        console.log(worldId)
+      }
+      
+    })
+  }
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -32,7 +44,7 @@ function App() {
 
   return (
     <>
-      {page === "landing" && <Landing onLogout={handleLogout} user={user} onNavigate={setPage}/>}
+      {page === "landing" && <Landing onLogout={handleLogout} user={user} onPlay={handlePlay} onNavigate={setPage}/>}
       {page === "play" && <Play user={user} onNavigate={setPage}/>} 
     </>
   )
