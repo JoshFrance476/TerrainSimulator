b8474b82d9ecc9269566b460178cfacbc7533a3b
Josh France <joshfrance.476@gmail.com>
2026-08-23 15:56:53 +0100

    Refactor: Consolidate story setup attributes into StorySetup class

--- backend/storytelling/story_state.py
+++ backend/storytelling/story_state.py
@@ -1,10 +1,13 @@
 import queue
+from models import StorySetup
 
 class StoryState:
     def __init__(self):
-        self.world_description = ""
-        self.character_description = ""  
-        self.story_focus_description = ""
+        self.story_setup = StorySetup(
+            world_description="",
+            character_description="",
+            story_focus_description=""
+        )
 
         self.character_notebook = []
         self.character_history = []
