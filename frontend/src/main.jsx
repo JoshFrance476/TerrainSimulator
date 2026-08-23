3cdb27e8c16f87c7be4281b0e0c0cc328374b7dd
Josh France <joshfrance.476@gmail.com>
2026-08-23 15:59:03 +0100

    Remove ReactQueryDevTools

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -19,7 +19,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 )
