3c64e3585aac69bde2dd956f7fbda158d4f68de2
Josh France <joshfrance.476@gmail.com>
2026-08-23 15:59:52 +0100

    Refactor usePlayer hook to simplify player location handling

--- frontend/src/hooks/usePlayer.jsx
+++ frontend/src/hooks/usePlayer.jsx
@@ -3,13 +3,7 @@ import { usePlayerQuery } from '../queries/queries'
 
 export function usePlayer() {
     const query = usePlayerQuery()
-    const location = query.data?.player_location
+    const location = query.data ?? null
 
-    // backend sends (row, col); frontend uses {x, y} = (col, row)
-    const playerLocation = useMemo(
-        () => (location ? { x: location[1], y: location[0] } : null),
-        [location?.[0], location?.[1]]
-    )
-
-    return { playerLocation, isLoading: query.isLoading, isError: query.isError }
+    return { playerLocation: location, isLoading: query.isLoading, isError: query.isError }
 }
