cd5931fd791f7c0ddce7f51fd7646bf49cfe5d40
Josh France <joshfrance.476@gmail.com>
2026-09-02 21:27:24 +0100

    Improve html structure

--- frontend/src/features/play-setup/WorldSetup.jsx
+++ frontend/src/features/play-setup/WorldSetup.jsx
@@ -1,19 +1,32 @@
 function WorldSetup({regionLookup, componentLookup}) {
     return (
-        <div className="modal-container-vertical">
+        <div className="world-setup-container">
             <div>
                 <h3>Regions</h3>
-                <div className="modal-list">
+                <div className="location-list">
                     {Object.entries(regionLookup).map(([key, region]) => (
-                        <div key={key}>{region.title} - {region.visible_description}</div>
+                        <div className="location-item" key={key}>
+                            <div className="location-title">{region.title}</div>
+                            <div className="location-visible-context">{region.visible_description}</div>
+                            {region.context && 
+                                <div className="location-hidden-context">{region.context}</div>
+                            }
+                        </div>
                     ))}
                 </div>
             </div>
             <div>
                 <h3>Components</h3>
-                <div className="modal-list">
+                <div className="location-list">
                     {Object.entries(componentLookup).map(([key, component]) => (
-                        <div key={key}>{component.name} - {component.description}</div>
+                        <div className="location-item" key={key}>
+                            <div className="location-title">{component.name}</div>
+                            <div className="location-visible-context">{component.description}</div>
+                            {component.context && 
+                                <div className="location-hidden-context">{component.context}</div>
+                            }
+                            
+                        </div>
                     ))}
                 </div>
             </div>
