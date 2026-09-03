d6b0e0861c09800c6a8db5663be69fc35bca9c65
Josh France <joshfrance.476@gmail.com>
2026-09-03 21:28:56 +0100

    Add player prop

--- frontend/src/features/play/InfoWindow.jsx
+++ frontend/src/features/play/InfoWindow.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import LocationTab from './info-tabs/LocationTab'
 import QuestTab from './info-tabs/QuestTab'
 import CharacterTab from './info-tabs/CharacterTab'
-function InfoWindow({ selectedCell }) {
+function InfoWindow({ selectedCell, player }) {
     const [activeTab, setActiveTab] = useState('location') // 'location', 'character','npcs','quests', "story"
     return (
         <div className="info-window">
@@ -33,7 +33,7 @@ function InfoWindow({ selectedCell }) {
                 </button>
             </div>
             {activeTab === 'location' && <LocationTab selectedCell={selectedCell} />}
-            {activeTab === 'character' && <CharacterTab />}
+            {activeTab === 'character' && <CharacterTab player={player} />}
             {activeTab === 'npcs' && (<div>
                 <p>NPC information will go here.</p>
             </div>
